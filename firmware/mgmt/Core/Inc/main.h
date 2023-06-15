fae76f7cb13c44265fe0f45311364468c1332340
Brett Regnier <bretternestregnier@gmail.com>
2023-06-15 17:02:48 -0600

    Updated pins for MGMT--Net and MGMT--UI, Net works, UI doesn't want ot upload

--- firmware/mgmt/Core/Inc/main.h
+++ firmware/mgmt/Core/Inc/main.h
@@ -70,10 +70,6 @@ void Error_Handler(void);
 #define ADC_UI_STAT_GPIO_Port GPIOA
 #define ADC_NET_STAT_Pin GPIO_PIN_1
 #define ADC_NET_STAT_GPIO_Port GPIOA
-#define UI_TX2_MGMT_Pin GPIO_PIN_2
-#define UI_TX2_MGMT_GPIO_Port GPIOA
-#define UI_RX2_MGMT_Pin GPIO_PIN_3
-#define UI_RX2_MGMT_GPIO_Port GPIOA
 #define LEDB_B_Pin GPIO_PIN_4
 #define LEDB_B_GPIO_Port GPIOA
 #define LEDA_B_Pin GPIO_PIN_6
@@ -85,22 +81,26 @@ void Error_Handler(void);
 #define RTS_Pin GPIO_PIN_1
 #define RTS_GPIO_Port GPIOB
 #define RTS_EXTI_IRQn EXTI0_1_IRQn
-#define USB_TX1_MGMT_Pin GPIO_PIN_10
-#define USB_TX1_MGMT_GPIO_Port GPIOB
-#define USB_RX1_MGMT_Pin GPIO_PIN_11
+#define USB_RX1_MGMT_Pin GPIO_PIN_10
 #define USB_RX1_MGMT_GPIO_Port GPIOB
+#define USB_TX1_MGMT_Pin GPIO_PIN_11
+#define USB_TX1_MGMT_GPIO_Port GPIOB
 #define CTS_Pin GPIO_PIN_13
 #define CTS_GPIO_Port GPIOB
 #define LEDB_G_Pin GPIO_PIN_14
 #define LEDB_G_GPIO_Port GPIOB
 #define LEDB_R_Pin GPIO_PIN_15
 #define LEDB_R_GPIO_Port GPIOB
-#define UI_STAT_Pin GPIO_PIN_9
-#define UI_STAT_GPIO_Port GPIOA
 #define NET_STAT_Pin GPIO_PIN_10
 #define NET_STAT_GPIO_Port GPIOA
 #define MGMT_DBG7_Pin GPIO_PIN_11
 #define MGMT_DBG7_GPIO_Port GPIOA
+#define UI_TX1_MGMT_Pin GPIO_PIN_3
+#define UI_TX1_MGMT_GPIO_Port GPIOA
+#define UI_RX1_MGMT_Pin GPIO_PIN_2
+#define UI_RX1_MGMT_GPIO_Port GPIOA
+#define UI_STAT_Pin GPIO_PIN_9
+#define UI_STAT_GPIO_Port GPIOA
 #define UI_RST_Pin GPIO_PIN_15
 #define UI_RST_GPIO_Port GPIOA
 #define UI_BOOT_Pin GPIO_PIN_3
@@ -109,27 +109,27 @@ void Error_Handler(void);
 #define NET_RST_GPIO_Port GPIOB
 #define NET_BOOT_Pin GPIO_PIN_5
 #define NET_BOOT_GPIO_Port GPIOB
-#define NET_TX0_MGMT_Pin GPIO_PIN_6
+#define NET_TX0_MGMT_Pin GPIO_PIN_7
 #define NET_TX0_MGMT_GPIO_Port GPIOB
-#define NET_RX0_MGMT_Pin GPIO_PIN_7
+#define NET_RX0_MGMT_Pin GPIO_PIN_6
 #define NET_RX0_MGMT_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
 #define USB_PORT GPIOB
-#define USB_RX_Bit GPIO_IDR_10
-#define USB_TX_Bit_On GPIO_ODR_11
-#define USB_TX_Bit_Off ~GPIO_ODR_11
+#define USB_RX_Bit GPIO_IDR_11
+#define USB_TX_Bit_On GPIO_ODR_10
+#define USB_TX_Bit_Off ~GPIO_ODR_10
 
 #define UI_PORT GPIOA
-#define UI_RX_Bit GPIO_IDR_3
-#define UI_TX_Bit_On GPIO_ODR_2
-#define UI_TX_Bit_Off ~GPIO_ODR_7
+#define UI_RX_Bit GPIO_IDR_3      // The input data register bit on mgmt
+#define UI_TX_Bit_On GPIO_ODR_2   // The output data register bit on mgmt
+#define UI_TX_Bit_Off ~GPIO_ODR_2
 
 #define NET_PORT GPIOB
-#define NET_RX_Bit GPIO_IDR_6
-#define NET_TX_Bit_On GPIO_ODR_7
-#define NET_TX_Bit_Off ~GPIO_ODR_7
+#define NET_RX_Bit GPIO_IDR_7     // The input data register bit on mgmt
+#define NET_TX_Bit_On GPIO_ODR_6  // The output data register bit on mgmt
+#define NET_TX_Bit_Off ~GPIO_ODR_6
 
 
 /* USER CODE END Private defines */
