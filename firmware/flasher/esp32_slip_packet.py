7265c2a5ffa3ef5dcc128dc373be2d0815d1cd66
Brett Regnier <bretternestregnier@gmail.com>
2023-09-14 17:42:05 -0600

    Working on fleshing out esp32 programming

--- firmware/flasher/esp32_slip_packet.py
+++ firmware/flasher/esp32_slip_packet.py
@@ -1,11 +1,9 @@
 import functools
+from copy import deepcopy
 
-
-def FromBytes(byte_arr: [] = []):
-    packet = esp32_slip_packet()
-
-    return packet
-
+# TODO decide on if I want to represent the data in big endian and then
+# encode it into big endian, or just have it little endian from the start???
+#
 
 class esp32_slip_packet:
     END = 0xC0
@@ -14,14 +12,13 @@ class esp32_slip_packet:
     ESC_ESC = 0xDD
 
     # Header is at least 8 bytes + 1 for end byte at start
-    data = [0] * 9
+    data = [0] * 8
     data_length = 0
 
     def __init__(self, request_response: int = 0, command: int = 0,
                  size: int = 0):
-        self.data[0] = self.END
+        # self.data[0] = self.END
         self.SetHeader(request_response, command, size)
-        pass
 
     def __getitem__(self, key):
         # -2 for the end byte
@@ -30,22 +27,64 @@ class esp32_slip_packet:
 
         return self.data[key-1]
 
+    def FromBytes(self, data: bytes):
+        if (data[0] != self.END and data[-1] != self.END):
+            raise Exception("Error. Start and end bytes missing")
+        idx = 1
+        # Remove slip encapsulation
+        cleaned_data = []
+        while idx < len(data):
+            if data[idx] == self.ESC:
+                if data[idx+1] == self.ESC_END:
+                    cleaned_data.append(self.END)
+                elif data[idx+1] == self.ESC_ESC:
+                    # following byte is not an escape
+                    cleaned_data.append(self.ESC)
+                else:
+                    raise Exception(f"{data[idx]} not properly escaped" +
+                                    f" at idx {idx}")
+                idx += 1
+            elif data[idx] == self.END:
+                print("end found")
+                break
+            else:
+                cleaned_data.append(data[idx])
+
+            idx += 1
+
+        if (len(cleaned_data) < 8):
+            raise Exception("Error. Data needs to be at least 8 bytes")
+
+        print(cleaned_data)
+
+        for i in range(8):
+            print(i)
+            self.data[i] = cleaned_data[i]
+
+        self.PushData(cleaned_data[8:8])
+
     def SetHeader(self, request_response: int, command: int, size: int = 0):
         # TODO error checking
-        self.data[1] = int(request_response).to_bytes(1, "little")
-        self.data[2] = command.to_bytes(1, "little")
+        self.data[0] = int(request_response).to_bytes(1, "little")[0]
+        self.data[1] = command.to_bytes(1, "little")[0]
         self.SetSize(size)
 
     def SetSize(self, size: int):
         data = size.to_bytes(2, "little")
-        self.data[3] = data[0]
-        self.data[4] = data[1]
+        self.data[2] = data[0]
+        self.data[3] = data[1]
+
+    def GetSize(self):
+        []
 
     def PushData(self, ele: int):
         # Convert to bytes and then append all of the bytes
         ele_bytes = ele.to_bytes((max(ele.bit_length(), 1) + 7) // 8, "little")
-        self.data_length += 1
-        self.data.push(ele_bytes)
+
+        for ele in ele_bytes:
+            self.data.append(ele)
+
+        self.data_length += len(ele_bytes)
 
     def PushDataArray(self, data_in: []):
         for ele in data_in:
@@ -54,15 +93,23 @@ class esp32_slip_packet:
     def Length(self):
         return self.data_length
 
-    def SetChecksum(self, arr: [int]):
-        checksum = functools.reduce(lambda a, b: a ^ b, arr)
-        checksum ^ self.CHECKSUM_SEED
+    def SetChecksum(self):
+        # Checksum seed
+        checksum = 0xEF
+        idx = 8
+        while (idx < self.data_length):
+
+            checksum ^= self.data[idx]
+            idx += 1
+
+        # checksum = functools.reduce(lambda a, b: a ^ b, self.data)
+        # checksum ^ self.CHECKSUM_SEED
         checksum_bytes = checksum.to_bytes(4, "little")
 
-        self.data[5] = checksum_bytes[0]
-        self.data[6] = checksum_bytes[1]
-        self.data[7] = checksum_bytes[2]
-        self.data[8] = checksum_bytes[3]
+        self.data[4] = checksum_bytes[0]
+        self.data[5] = checksum_bytes[1]
+        self.data[6] = checksum_bytes[2]
+        self.data[7] = checksum_bytes[3]
 
     def SLIPEncode(self, checksum: bool = False):
         # First check if there is END
@@ -75,12 +122,13 @@ class esp32_slip_packet:
         self.SetSize(self.data_length)
 
         # TODO might change?
-        encoded_data = self.data
-        data_len = len(encoded_data)
+        # encoded_data = deepcopy(self.data)
+        encoded_data = []
+        encoded_data.append(self.END)
 
         # Skip the first byte
         idx = 1
-        while (idx < data_len):
+        while (idx < len(self.data)):
             if self.END == encoded_data[idx]:
                 encoded_data[idx] = self.ESC
                 encoded_data.insert(idx+1, self.ESC_END)
@@ -88,10 +136,11 @@ class esp32_slip_packet:
             elif self.ESC == encoded_data[idx]:
                 encoded_data.insert(idx+1, self.ESC_ESC)
                 idx += 1
+            encoded_data.append(self.data[idx])
             idx += 1
 
         # Put on the final end byte
-        encoded_data.push(self.END)
+        encoded_data.append(self.END)
 
         return bytes(encoded_data)
 
--- firmware/flasher/flasher.py
+++ firmware/flasher/flasher.py
@@ -3,6 +3,7 @@ import serial
 import uart_utils
 import stm32
 import esp32
+import esp32_slip_packet
 
 BGY = "\033[1;30m"
 BR = "\033[1;31m"
@@ -44,10 +45,15 @@ def main():
         print(f"Opened port: {port} baudrate: {baud}")
 
         # print(f"{B_WHITE}Starting{N_WHITE}")
-        stm32_flasher = stm32.stm32_flasher(uart)
-        stm32_flasher.ProgramSTM()
+        # stm32_flasher = stm32.stm32_flasher(uart)
+        # stm32_flasher.ProgramSTM()
 
         # esp32_flasher = esp32.esp32_flasher(uart)
+        # esp32_flasher.ProgramESP()
+
+        data = bytes([0xc0, 0xdb, 0xdc, 0xdb, 0xdc, 0xdb, 0xdd, 0xdd, 0x00, 0x00, 0x00, 0x00, 0xc0])
+        packet = esp32_slip_packet.esp32_slip_packet()
+        packet.FromBytes(data)
 
         uart.close()
     except Exception as ex:
