469cf964d91b1062ca5ed9cd2dd30109ff04f1a7
suhasHere <snandaku@cisco.com>
2023-10-13 22:17:00 -0700

    tidy-up qchat encode/decode

--- firmware/ui/src/QChat.cc
+++ firmware/ui/src/QChat.cc
@@ -4,20 +4,15 @@ namespace qchat
 {
 uint16_t encode(Packet* packet, const WatchRoom& msg)
 {
-    // Set the length of the entire message
-    packet->AppendData(
-        msg.publisher_uri.length() + msg.room_uri.length() + Str_Len_Bytes,
-        10);
-
-    packet->AppendData(msg.publisher_uri.length(), Str_Len_Bits);
-
-    for (uint16_t i = 0; i < msg.publisher_uri.length(); ++i)
+    // [type][pub_uri_len][[pub_uri][room_uri_len][room_uri]
+    packet->AppendData(MessageTypes::Watch, 8);
+    packet->AppendData(msg.publisher_uri.length(), uri_len_bits);
+    for (size_t i = 0; i < msg.publisher_uri.length(); ++i)
     {
         packet->AppendData(msg.publisher_uri[i], 8);
     }
 
-    packet->AppendData(msg.room_uri.length(), Str_Len_Bits);
-
+    packet->AppendData(msg.room_uri.length(), uri_len_bits);
     for (uint16_t i = 0; i < msg.room_uri.length(); ++i)
     {
         packet->AppendData(msg.room_uri[i], 8);
@@ -26,73 +21,87 @@ uint16_t encode(Packet* packet, const WatchRoom& msg)
 
 void encode(Packet* packet, const uint16_t start_offset, const Ascii& msg)
 {
-    // Set the length of the entire message
-    packet->AppendData(
-        msg.message.length() + msg.message_uri.length() + Str_Len_Bytes,
-        10);
-
-    packet->AppendData(msg.message.length(), Str_Len_Bits);
-
-    for (uint16_t i = 0; i < msg.message.length(); ++i)
-    {
-        packet->AppendData(msg.message[i], 8);
-    }
-
-    packet->AppendData(msg.message_uri.length(), Str_Len_Bits);
-
-    for (uint16_t i = 0; i < msg.message_uri.length(); ++i)
+    // [type][msg_uri_len][[msg_uri][msg_len][msg]
+    packet->AppendData(MessageTypes::Ascii, 8);
+    packet->AppendData(msg.message_uri.length(), uri_len_bits);
+    for (size_t i = 0; i < msg.message_uri.length(); ++i)
     {
         packet->AppendData(msg.message_uri[i], 8);
     }
+    packet->AppendData(msg.message.length(), msg_len_bits);
+    for (size_t i = 0; i < msg.message.length(); ++i)
+    {
+        packet->AppendData(msg.message[i], 8);
+    }
 }
 
-void decode(WatchRoom& msg, const Packet& encoded, const uint16_t start_addr)
+bool decode(WatchRoom& msg, const Packet& encoded, size_t& current_offset)
 {
-    // Get the publisher_uri len
-    uint16_t offset = start_addr;
-    uint16_t len = encoded.GetData(offset, Str_Len_Bits);
+    // type
+    size_t offset = current_offset;
+    MessageTypes msg_type = static_cast<MessageTypes>(encoded.GetData(offset, 8));
+    if (msg_type != MessageTypes::Watch) {
+        return false;
+    }
 
-    offset += Str_Len_Bits;
-    for (uint16_t i = 0; i < len; ++i)
+    // Get the publisher_uri len
+    size_t offset += 8;
+    size_t uri_len = encoded.GetData(offset, uri_len_bits);
+    offset += uri_len_bits;
+    for (size_t i = 0; i < uri_len; ++i)
     {
         msg.publisher_uri.push_back(static_cast<char>(
             encoded.GetData(offset, 8)));
         offset += 8;
     }
 
-    len = encoded.GetData(offset, Str_Len_Bits);
-    offset += Str_Len_Bits;
-
-    for (uint16_t i = 0; i < len; ++i)
+    // room_uri
+    uri_len = encoded.GetData(offset, uri_len_bits);
+    offset += uri_len_bits;
+    for (uint16_t i = 0; i < uri_len; ++i)
     {
         msg.room_uri.push_back(static_cast<char>(
             encoded.GetData(offset, 8)));
         offset += 8;
     }
+
+    current_offset = offset;
+    return true;
 }
 
-void decode(Ascii& msg, const Packet& encoded, const uint16_t start_addr)
+bool decode(Ascii& msg, const Packet& encoded, size_t& current_offset)
 {
-    uint16_t offset = start_addr;
-    uint16_t len = encoded.GetData(offset, Str_Len_Bits);
-
-    offset += Str_Len_Bits;
-    for (uint16_t i = 0; i < len; ++i)
-    {
-        msg.message.push_back(static_cast<char>(
-            encoded.GetData(offset, 8)));
-        offset += 8;
+    // type
+    size_t offset = current_offset;
+    MessageTypes msg_type = static_cast<MessageTypes>(encoded.GetData(offset, 8));
+    if (msg_type != MessageTypes::Ascii) {
+        return false;
     }
 
-    len = encoded.GetData(offset, Str_Len_Bits);
-    offset += Str_Len_Bits;
-
-    for (uint16_t i = 0; i < len; ++i)
+    // message uri
+    offset += 8;
+    size_t uri_len = encoded.GetData(offset, uri_len_bits);
+    offset += uri_len_bits;
+    for (uint16_t i = 0; i < uri_len; ++i)
     {
         msg.message_uri.push_back(static_cast<char>(
             encoded.GetData(offset, 8)));
         offset += 8;
     }
+
+    // msg
+    size_t msg_len = encoded.GetData(offset, msg_len_bits);
+    offset += msg_len_bits;
+
+    for (uint16_t i = 0; i < msg_len; ++i)
+    {
+        msg.message.push_back(static_cast<char>(
+            encoded.GetData(offset, 8)));
+        offset += 8;
+    }
+    
+    current_offset = offset;
+    return true;
 }
 
 } // namespace qchat
