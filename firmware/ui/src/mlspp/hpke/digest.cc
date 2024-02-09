47ee7b94d55f52400b3094966f96cd85330d21c1
Richard Barnes <rlb@ipv.sx>
2024-02-09 15:14:22 -0500

    Add STM crypto library

--- firmware/ui/src/mlspp/hpke/digest.cc
+++ firmware/ui/src/mlspp/hpke/digest.cc
@@ -1,11 +1,30 @@
 #include <hpke/digest.h>
 #include <namespace.h>
+#include <crypto/hash/cmox_hash.h>
 
 namespace MLS_NAMESPACE::hpke {
 
+cmox_hash_algo_t
+cmox_hash_id(Digest::ID id) {
+  switch (id) {
+    case Digest::ID::SHA256: return CMOX_SHA256_ALGO;
+    case Digest::ID::SHA384: return CMOX_SHA384_ALGO;
+    case Digest::ID::SHA512: return CMOX_SHA512_ALGO;
+  }
+}
+
+size_t
+cmox_hash_size(Digest::ID id) {
+  switch (id) {
+    case Digest::ID::SHA256: return 32;
+    case Digest::ID::SHA384: return 48;
+    case Digest::ID::SHA512: return 64;
+  }
+}
+
 Digest::Digest(Digest::ID id_in)
   : id(id_in)
-  , hash_size(0)
+  , hash_size(cmox_hash_size(id_in))
 {
 }
 
@@ -13,6 +32,16 @@ bytes
 Digest::hash(const bytes& data) const
 {
   auto md = bytes(hash_size);
+
+  // TODO check return values and throw on error
+  auto digest_len = size_t(0);
+  cmox_hash_compute(cmox_hash_id(id),
+                    data.data(),
+                    data.size(),
+                    md.data(),
+                    md.size(),
+                    &digest_len);
+
   return md;
 }
 
