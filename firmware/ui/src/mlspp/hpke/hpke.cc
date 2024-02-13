f22d635f0d32e2ff4c69cf7f58801ccb68ce20ca
Richard Barnes <rlb@ipv.sx>
2024-02-13 13:13:57 -0500

    Prevent one common.cc from shadowing the other

--- firmware/ui/src/mlspp/hpke/hpke.cc
+++ firmware/ui/src/mlspp/hpke/hpke.cc
@@ -194,19 +194,6 @@ struct P256 : KEM {
   const KDF& kdf;
   const bytes suite_id = {};
 
-  static bytes i2osp(uint64_t val, size_t size) {
-    auto out = bytes(size, 0);
-    auto max = size;
-    if (size > 8) {
-      max = 8;
-    }
-
-    for (size_t i = 0; i < max; i++) {
-      out.at(size - i - 1) = static_cast<uint8_t>(val >> (8 * i));
-    }
-    return out;
-  }
-
   static bytes make_suite_id(ID id) {
     static const auto label_kem = from_ascii("KEM");
     return label_kem + i2osp(uint16_t(id), 2);
