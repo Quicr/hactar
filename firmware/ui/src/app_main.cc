d216b43edc541fdb63b57be9872448d2afedc29c
Richard Barnes <rlb@ipv.sx>
2024-02-15 14:54:12 -0500

    Write to screen; only run crypto tests once

--- firmware/ui/src/app_main.cc
+++ firmware/ui/src/app_main.cc
@@ -79,7 +79,6 @@ uint16_t GenerateTriangleWavePoint(double frequency, double amplitude, double ti
 }
 
 std::string space_separated_line(std::stringstream&& str) {
-  str << std::endl;
   return str.str();
 }
 
@@ -94,9 +93,127 @@ void log(const T&... args) {
   auto str = std::stringstream();
   str << "[UI] ";
   const auto line = space_separated_line(std::move(str), args...);
-  const auto* line_ptr = reinterpret_cast<const uint8_t*>(line.c_str());
 
-  HAL_UART_Transmit(&huart1, line_ptr, line.size(), HAL_MAX_DELAY);
+  // Uncomment for UART logging
+  // line = line + "\n";
+  // const auto* line_ptr = reinterpret_cast<const uint8_t*>(line.c_str());
+  // HAL_UART_Transmit(&huart1, line_ptr, line.size(), HAL_MAX_DELAY);
+
+  // Uncomment for logging to screen
+  static int y = 0;
+  screen.DrawText(0, y, line.c_str(), font7x12, C_GREEN, C_BLACK);
+  y += 12;
+}
+
+// import hashlib
+// hashlib.sha256(b"hello").hexdigest()
+void test_digest() {
+    using namespace mls::hpke;
+    const auto digest = Digest::get<Digest::ID::SHA256>();
+    const auto data = from_ascii("hello");
+
+    const auto output = digest.hash(data);
+
+    const auto expected = from_hex("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824");
+    const auto pass = output == expected;
+    const auto status = std::string(pass? "PASS" : "FAIL");
+    log("hash", status);
+}
+
+void test_hmac() {
+    using namespace mls::hpke;
+    const auto digest = Digest::get<Digest::ID::SHA256>();
+    const auto key = from_ascii("key");
+    const auto data = from_ascii("hello");
+
+    const auto output = digest.hmac(key, data);
+
+    const auto expected = from_hex("9307b3b915efb5171ff14d8cb55fbcc798c6c0ef1456d66ded1a6aa723a58b7b");
+    const auto pass = output == expected;
+    const auto status = std::string(pass? "PASS" : "FAIL");
+    log("hmac", status);
+}
+
+void test_aead() {
+    try {
+        using namespace mls::hpke;
+        const auto& cipher = AEAD::get<AEAD::ID::AES_128_GCM>();
+        const auto key = from_ascii("sixteen byte key");
+        const auto nonce = from_ascii("public nonce");
+        const auto pt = from_ascii("secret message");
+        const auto aad = from_ascii("extra");
+        log("cipher", "prep");
+
+        const auto ct = cipher.seal(key, nonce, aad, pt);
+        log("cipher", "seal", to_hex(ct));
+
+        const auto expected = from_hex("76e196daeff5e2224f12f7726d82aaedbe176f85ece437c5ce7861a02fc2");
+        const auto pass_kat = (ct == expected);
+        log("cipher", "KAT", std::string(pass_kat? "PASS" : "FAIL"));
+
+        const auto maybe_pt = cipher.open(key, nonce, aad, ct);
+        log("cipher", "open");
+
+        auto pt_print = std::string("");
+        if (maybe_pt) {
+          pt_print = to_hex(*maybe_pt);
+        }
+        log("cipher", "pt", pt_print);
+
+        const auto pass_rtt = maybe_pt && pt == *maybe_pt;
+        log("cipher", "RTT", std::string(pass_rtt? "PASS" : "FAIL"));
+    } catch (const std::exception& e) {
+        log("cipher", "throw", e.what());
+    }
+}
+
+void test_sig() {
+    try {
+        using namespace mls::hpke;
+        const auto& sig = Signature::get<Signature::ID::P256_SHA256>();
+        const auto msg = from_ascii("attack at dawn!");
+
+        const auto sk = sig.generate_key_pair();
+        log("sig", "keygen");
+
+        const auto pk = sk->public_key();
+        log("sig", "public_key");
+
+        const auto sig_val = sig.sign(msg, *sk);
+        log("sig", "sign", sig_val.size());
+
+        const auto ver = sig.verify(msg, sig_val, *pk);
+        log("sig", "verify", ver);
+
+        const auto pass = ver;
+        log("sig", "pass", pass);
+    } catch (const std::exception& e) {
+        log("sig", "throw", e.what());
+    }
+}
+
+void test_kem() {
+    try {
+        using namespace mls::hpke;
+        const auto& kem = KEM::get<KEM::ID::DHKEM_P256_SHA256>();
+
+        const auto sk = kem.generate_key_pair();
+        log("kem", "keygen");
+
+        const auto pk = sk->public_key();
+        log("kem", "public_key");
+
+        const auto [zz_send, enc] = kem.encap(*pk);
+        log("kem", "encap", zz_send.size(), enc.size());
+
+        const auto zz_recv = kem.decap(enc, *sk);
+        log("kem", "decap", zz_recv.size());
+
+        const auto pass = zz_send == zz_recv;
+        log("kem", "pass", pass);
+    } catch (const std::exception& e) {
+        log("kem", "throw", e.what());
+    }
 }
 
 // TODO Get the osc working correctly from an external signal
@@ -156,10 +273,10 @@ int app_main()
 
     // TODO figure out how to send a small sound??
     // Generate a simple triangle wave
-    double frequency = 1000; // Hz
-    double amplitude = 1000.0;
-    double duration = 1.0;  //seconds
-    double sample_rate = 16'000; // samples per second5
+    // double frequency = 1000; // Hz
+    // double amplitude = 1000.0;
+    // double duration = 1.0;  //seconds
+    // double sample_rate = 16'000; // samples per second5
     uint32_t i = 0;
     // for (double t = 0.0; t < duration && i < SOUND_BUFFER_SZ; t += 1.0 / sample_rate)
     // {
@@ -180,7 +297,7 @@ int app_main()
 
     while (i < SOUND_BUFFER_SZ)
     {
-        for (int j = 0; j < sizeof(sine_wave) / sizeof(sine_wave[0]); ++j)
+        for (unsigned int j = 0; j < sizeof(sine_wave) / sizeof(sine_wave[0]); ++j)
         {
             tx_sound_buff[i++] = sine_wave[j];
         }
@@ -209,9 +326,10 @@ int app_main()
     // tx_sound_buff[SOUND_BUFFER_SZ - 2] = 0x00AA;
 
     // Delayed condition
+    auto first_run = false;
     uint32_t blink = HAL_GetTick() + 5000;
     uint32_t tx_sound = 0;
-    auto output = HAL_I2S_Transmit_DMA(&hi2s3, tx_sound_buff, SOUND_BUFFER_SZ * sizeof(uint16_t));
+    // auto output = HAL_I2S_Transmit_DMA(&hi2s3, tx_sound_buff, SOUND_BUFFER_SZ * sizeof(uint16_t));
     while (1)
     {
         ui_manager->Run();
@@ -224,8 +342,8 @@ int app_main()
             // screen.DrawText(0, 100, String::int_to_string((int)tx_sound_buff[0]), font7x12, C_GREEN, C_BLACK);
 
             // audio->TestRegister();
-            uint16_t reg_value = 0;
-            uint16_t value = 0x01FF;
+            // uint16_t reg_value = 0;
+            // uint16_t value = 0x01FF;
             // audio->ReadRegister(0x0A, reg_value);
             // screen.DrawText(0, 100, String::int_to_string((int)reg_value), font7x12, C_GREEN, C_BLACK);
 
@@ -260,160 +378,20 @@ int app_main()
             // HAL_RNG_GenerateRandomNumber(&hrng, &num);
             // screen.DrawText(0, 82, String::int_to_string(num), font7x12, C_GREEN, C_BLACK);
 
-            // Verify that try/catch works properly
-            try {
-              log("try/catch", "throw");
-              throw std::runtime_error("fnord");
-            } catch (const std::exception& e) {
-              log("try/catch", "caught", e.what());
+            // Self-test the crypto on the first run-through
+            if (first_run) {
+              test_digest();
+              test_hmac();
+              test_aead();
+              test_sig();
+              test_kem();
+              first_run = false;
             }
 
-#if 0
-            // Compute and display a digest
-            //
-            // import hashlib
-            // hashlib.sha256(b"hello").hexdigest()
-            {
-                using namespace mls::hpke;
-                const auto digest = Digest::get<Digest::ID::SHA256>();
-                const auto data = from_ascii("hello");
-                const auto output = digest.hash(data);
-                const auto expected = from_hex("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824");
-
-                const auto pass = output == expected;
-
-                const auto label = std::string("hash");
-                const auto status = std::string(pass? "PASS" : "FAIL");
-                const auto line = label + ": " + status;
-                screen.DrawText(0, 82, line.c_str(), font7x12, C_GREEN, C_BLACK);
-            }
-
-            // Compute and display a MAC
-            //
-            // import hashlib, hmac
-            // hmac.HMAC(b"key", b"hello", digestmod=hashlib.sha256).hexdigest()[:8]
-            {
-                using namespace mls::hpke;
-                const auto digest = Digest::get<Digest::ID::SHA256>();
-                const auto key = from_ascii("key");
-                const auto data = from_ascii("hello");
-                const auto output = digest.hmac(key, data);
-                const auto expected = from_hex("9307b3b915efb5171ff14d8cb55fbcc798c6c0ef1456d66ded1a6aa723a58b7b");
-
-                const auto pass = output == expected;
-
-                const auto label = std::string("hmac");
-                const auto status = std::string(pass? "PASS" : "FAIL");
-                const auto line = label + ": " + status;
-                screen.DrawText(0, 100, line.c_str(), font7x12, C_GREEN, C_BLACK);
-            }
-
-            // Do an encrypt/decrypt round-trip test
-            try {
-                using namespace mls::hpke;
-                const auto& cipher = AEAD::get<AEAD::ID::AES_128_GCM>();
-                const auto key = from_ascii("sixteen byte key");
-                const auto nonce = from_ascii("public nonce");
-                const auto pt = from_ascii("secret message");
-                const auto aad = from_ascii("extra");
-                log("cipher", "prep");
-
-                const auto ct = cipher.seal(key, nonce, aad, pt);
-                log("cipher", "seal", to_hex(ct));
-
-                {
-                  const auto expected = from_hex("76e196daeff5e2224f12f7726d82aaedbe176f85ece437c5ce7861a02fc2");
-                  const auto pass = (ct == expected);
-
-                  const auto label = std::string("aead KAT");
-                  const auto status = std::string(pass? "PASS" : "FAIL");
-                  const auto line = label + ": " + status;
-                  screen.DrawText(0, 118, line.c_str(), font7x12, C_GREEN, C_BLACK);
-                }
-
-                const auto maybe_pt = cipher.open(key, nonce, aad, ct);
-                log("cipher", "open");
-
-                auto pt_print = std::string("");
-                if (maybe_pt) {
-                  pt_print = to_hex(*maybe_pt);
-                }
-                log("cipher", "pt", pt_print);
-
-                const auto pass = maybe_pt && pt == *maybe_pt;
-                log("cipher open", "pass", pass);
-
-                const auto label = std::string("aead RTT");
-                const auto status = std::string(pass? "PASS" : "FAIL");
-                const auto line = label + ": " + status;
-                screen.DrawText(0, 118, line.c_str(), font7x12, C_GREEN, C_BLACK);
-            } catch (const std::exception& e) {
-                log("cipher", "throw", e.what());
-            }
-#endif // 0
-            // Do a sign/verify round-trip test
-            try {
-                using namespace mls::hpke;
-                const auto& sig = Signature::get<Signature::ID::P256_SHA256>();
-                const auto msg = from_ascii("attack at dawn!");
-
-                const auto sk = sig.generate_key_pair();
-                log("sig", "keygen");
-
-                const auto pk = sk->public_key();
-                log("sig", "public_key");
-
-                const auto sig_val = sig.sign(msg, *sk);
-                log("sig", "sign", sig_val.size());
-
-                const auto ver = sig.verify(msg, sig_val, *pk);
-                log("sig", "verify", ver);
-
-                const auto pass = ver;
-                log("sig", "pass", pass);
-
-                const auto label = std::string("sig");
-                const auto status = std::string(pass? "PASS" : "FAIL");
-                const auto line = label + ": " + status;
-                screen.DrawText(0, 136, line.c_str(), font7x12, C_GREEN, C_BLACK);
-            } catch (const std::exception& e) {
-                log("sig", "throw", e.what());
-            }
-
-            // Do an encap/decap round-trip test
-            try {
-                using namespace mls::hpke;
-                const auto& kem = KEM::get<KEM::ID::DHKEM_P256_SHA256>();
-
-                const auto sk = kem.generate_key_pair();
-                log("kem", "keygen");
-
-                const auto pk = sk->public_key();
-                log("kem", "public_key");
-
-                const auto [zz_send, enc] = kem.encap(*pk);
-                log("kem", "encap", zz_send.size(), enc.size());
-
-                const auto zz_recv = kem.decap(enc, *sk);
-                log("kem", "decap", zz_recv.size());
-
-                const auto pass = zz_send == zz_recv;
-                log("kem", "pass", pass);
-
-                const auto label = std::string("kem");
-                const auto status = std::string(pass? "PASS" : "FAIL");
-                const auto line = label + ": " + status;
-                screen.DrawText(0, 154, line.c_str(), font7x12, C_GREEN, C_BLACK);
-            } catch (const std::exception& e) {
-                log("kem", "throw", e.what());
-            }
-
-            // HAL_GPIO_TogglePin(UI_LED_R_GPIO_Port, UI_LED_R_Pin);
-            //     uint8_t test_message [] = "UI: Test\n\r";
-            //     HAL_UART_Transmit(&huart1, test_message, 10, 1000);
-
-            if (rx_busy)
+            if (rx_busy) {
                 continue;
+            }
+
             // We need this because if the mic stays on it will write to USART3 in
             // bootloader mode which locks up the main chip
             // TODO remove enable mic bits [2,3]
@@ -461,7 +439,7 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef* huart)
     }
 }
 
-void HAL_I2SEx_TxRxCpltCallback(I2S_HandleTypeDef* hi2s)
+void HAL_I2SEx_TxRxCpltCallback(I2S_HandleTypeDef* /* hi2s */)
 {
     // audio->RxComplete();
     // rx_busy = false;
@@ -470,9 +448,9 @@ void HAL_I2SEx_TxRxCpltCallback(I2S_HandleTypeDef* hi2s)
     HAL_GPIO_TogglePin(UI_LED_B_GPIO_Port, UI_LED_B_Pin);
 }
 
-void HAL_I2S_TxCpltCallback(I2S_HandleTypeDef* hi2s)
+void HAL_I2S_TxCpltCallback(I2S_HandleTypeDef* /* hi2s */)
 {
-    auto output = HAL_I2S_Transmit_DMA(&hi2s3, tx_sound_buff, SOUND_BUFFER_SZ * sizeof(uint16_t));
+    // auto output = HAL_I2S_Transmit_DMA(&hi2s3, tx_sound_buff, SOUND_BUFFER_SZ * sizeof(uint16_t));
 
     HAL_GPIO_TogglePin(UI_LED_G_GPIO_Port, UI_LED_G_Pin);
 }
