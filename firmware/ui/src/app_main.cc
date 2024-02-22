f231c4f2a46870cb2170df887872f8a2745c3162
Richard Barnes <rlb@ipv.sx>
2024-02-22 11:49:09 -0500

    Use heap-allocated instead of stack-allocated storage

--- firmware/ui/src/app_main.cc
+++ firmware/ui/src/app_main.cc
@@ -149,52 +149,25 @@ bool test_hmac(Logger& log) {
 
 bool test_aead_raw(Logger& log) {
   // Constants
-  const uint8_t Key[] =
-  {
-    0x46, 0x3b, 0x41, 0x29, 0x11, 0x76, 0x7d, 0x57, 0xa0, 0xb3, 0x39, 0x69, 0xe6, 0x74, 0xff, 0xe7,
-    0x84, 0x5d, 0x31, 0x3b, 0x88, 0xc6, 0xfe, 0x31, 0x2f, 0x3d, 0x72, 0x4b, 0xe6, 0x8e, 0x1f, 0xca
-  };
-  const uint8_t IV[] =
-  {
-    0x61, 0x1c, 0xe6, 0xf9, 0xa6, 0x88, 0x07, 0x50, 0xde, 0x7d, 0xa6, 0xcb
-  };
-  const uint8_t Plaintext[] =
-  {
-    0xe7, 0xd1, 0xdc, 0xf6, 0x68, 0xe2, 0x87, 0x68, 0x61, 0x94, 0x0e, 0x01, 0x2f, 0xe5, 0x2a, 0x98,
-    0xda, 0xcb, 0xd7, 0x8a, 0xb6, 0x3c, 0x08, 0x84, 0x2c, 0xc9, 0x80, 0x1e, 0xa5, 0x81, 0x68, 0x2a,
-    0xd5, 0x4a, 0xf0, 0xc3, 0x4d, 0x0d, 0x7f, 0x6f, 0x59, 0xe8, 0xee, 0x0b, 0xf4, 0x90, 0x0e, 0x0f,
-    0xd8, 0x50, 0x42
-  };
-  const uint8_t AddData[] =
-  {
-    0x0a, 0x68, 0x2f, 0xbc, 0x61, 0x92, 0xe1, 0xb4, 0x7a, 0x5e, 0x08, 0x68, 0x78, 0x7f, 0xfd, 0xaf,
-    0xe5, 0xa5, 0x0c, 0xea, 0xd3, 0x57, 0x58, 0x49, 0x99, 0x0c, 0xdd, 0x2e, 0xa9, 0xb3, 0x59, 0x77,
-    0x49, 0x40, 0x3e, 0xfb, 0x4a, 0x56, 0x68, 0x4f, 0x0c, 0x6b, 0xde, 0x35, 0x2d, 0x4a, 0xee, 0xc5
-  };
-  const uint8_t Expected_Ciphertext[] =
-  {
-    0x88, 0x86, 0xe1, 0x96, 0x01, 0x0c, 0xb3, 0x84, 0x9d, 0x9c, 0x1a, 0x18, 0x2a, 0xbe, 0x1e, 0xea,
-    0xb0, 0xa5, 0xf3, 0xca, 0x42, 0x3c, 0x36, 0x69, 0xa4, 0xa8, 0x70, 0x3c, 0x0f, 0x14, 0x6e, 0x8e,
-    0x95, 0x6f, 0xb1, 0x22, 0xe0, 0xd7, 0x21, 0xb8, 0x69, 0xd2, 0xb6, 0xfc, 0xd4, 0x21, 0x6d, 0x7d,
-    0x4d, 0x37, 0x58,
-  };
-  const uint8_t Expected_Tag[] =
-  {
-    0x24, 0x69, 0xce, 0xcd, 0x70, 0xfd, 0x98, 0xfe, 0xc9, 0x26, 0x4f, 0x71, 0xdf, 0x1a, 0xee, 0x9a
-  };
+  const auto Key = from_hex("463b412911767d57a0b33969e674ffe7845d313b88c6fe312f3d724be68e1fca");
+  const auto IV = from_hex("611ce6f9a6880750de7da6cb");
+  const auto Plaintext = from_hex("e7d1dcf668e2876861940e012fe52a98dacbd78ab63c08842cc9801ea581682ad54af0c34d0d7f6f59e8ee0bf4900e0fd85042");
+  const auto AddData = from_hex("0a682fbc6192e1b47a5e0868787ffdafe5a50cead3575849990cdd2ea9b3597749403efb4a56684f0c6bde352d4aeec5");
+  const auto Expected_Ciphertext = from_hex("8886e196010cb3849d9c1a182abe1eeab0a5f3ca423c3669a4a8703c0f146e8e956fb122e0d721b869d2b6fcd4216d7d4d3758");
+  const auto Expected_Tag = from_hex("2469cecd70fd98fec9264f71df1aee9a");
 
-  uint8_t Computed_Ciphertext[sizeof(Expected_Ciphertext) + sizeof(Expected_Tag)];
-  uint8_t Computed_Plaintext[sizeof(Plaintext)];
+  auto Computed_Ciphertext = bytes(Expected_Ciphertext.size() + Expected_Tag.size());
+  auto Computed_Plaintext = bytes(Plaintext.size());
   size_t computed_size = 0;
 
   // Encrypt
   const auto erv = cmox_aead_encrypt(CMOX_AES_GCM_ENC_ALGO,                  /* Use AES GCM algorithm */
-                             Plaintext, sizeof(Plaintext),           /* Plaintext to encrypt */
-                             sizeof(Expected_Tag),                   /* Authentication tag size */
-                             Key, sizeof(Key),                       /* AES key to use */
-                             IV, sizeof(IV),                         /* Initialization vector */
-                             AddData, sizeof(AddData),               /* Additional authenticated data */
-                             Computed_Ciphertext, &computed_size);   /* Data buffer to receive generated ciphertext
+                             Plaintext.data(), Plaintext.size(),           /* Plaintext to encrypt */
+                             Expected_Tag.size(),                   /* Authentication tag size */
+                             Key.data(), Key.size(),                       /* AES key to use */
+                             IV.data(), IV.size(),                         /* Initialization vector */
+                             AddData.data(), AddData.size(),               /* Additional authenticated data */
+                             Computed_Ciphertext.data(), &computed_size);   /* Data buffer to receive generated ciphertext
                                                                         and authentication tag */
   log.log("aead_raw", "encrypt");
 
@@ -204,31 +177,31 @@ bool test_aead_raw(Logger& log) {
     return false;
   }
 
-  if (computed_size != (sizeof(Expected_Ciphertext) + sizeof(Expected_Tag)))
+  if (computed_size != Expected_Ciphertext.size() + Expected_Tag.size())
   {
     log.log("aead_raw", "computed_size", computed_size);
     return false;
   }
 
-  if (memcmp(Expected_Ciphertext, Computed_Ciphertext, sizeof(Expected_Ciphertext)) != 0)
+  if (memcmp(Expected_Ciphertext.data(), Computed_Ciphertext.data(), Expected_Ciphertext.size()) != 0)
   {
     log.log("aead_raw", "memcmp ct");
     return false;
   }
 
-  if (memcmp(Expected_Tag, &Computed_Ciphertext[sizeof(Expected_Ciphertext)], sizeof(Expected_Tag)) != 0)
+  if (memcmp(Expected_Tag.data(), Computed_Ciphertext.data() + Expected_Ciphertext.size(), Expected_Tag.size()) != 0)
   {
     log.log("aead_raw", "memcmp tag");
     return false;
   }
 
   const auto drv = cmox_aead_decrypt(CMOX_AES_GCM_DEC_ALGO,                  /* Use AES GCM algorithm */
-                             Computed_Ciphertext, computed_size,     /* Ciphertext + tag to decrypt and verify */
-                             sizeof(Expected_Tag),                   /* Authentication tag size */
-                             Key, sizeof(Key),                       /* AES key to use */
-                             IV, sizeof(IV),                         /* Initialization vector */
-                             AddData, sizeof(AddData),               /* Additional authenticated data */
-                             Computed_Plaintext, &computed_size);    /* Data buffer to receive generated plaintext */
+                             Computed_Ciphertext.data(), computed_size,     /* Ciphertext + tag to decrypt and verify */
+                             Expected_Tag.size(),                   /* Authentication tag size */
+                             Key.data(), Key.size(),                       /* AES key to use */
+                             IV.data(), IV.size(),                         /* Initialization vector */
+                             AddData.data(), AddData.size(),               /* Additional authenticated data */
+                             Computed_Plaintext.data(), &computed_size);    /* Data buffer to receive generated plaintext */
   log.log("aead_raw", "decrypt");
 
   /* Verify API returned value */
@@ -239,14 +212,14 @@ bool test_aead_raw(Logger& log) {
   }
 
   /* Verify generated data size is the expected one */
-  if (computed_size != sizeof(Plaintext))
+  if (computed_size != Plaintext.size())
   {
     log.log("aead_raw", "computed_size", computed_size);
     return false;
   }
 
   /* Verify generated data are the expected ones */
-  if (memcmp(Plaintext, Computed_Plaintext, computed_size) != 0)
+  if (memcmp(Plaintext.data(), Computed_Plaintext.data(), computed_size) != 0)
   {
     log.log("aead_raw", "memcmp");
     return false;
