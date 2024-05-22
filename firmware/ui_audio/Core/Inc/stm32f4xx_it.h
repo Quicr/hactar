449c45bc6e4c86659b62288c7fd6e08e0c207ae0
Brett Regnier <bretternestregnier@gmail.com>
2024-05-22 13:46:31 -0600

    Added sine wave generation

--- firmware/ui_audio/Core/Inc/stm32f4xx_it.h
+++ firmware/ui_audio/Core/Inc/stm32f4xx_it.h
@@ -57,6 +57,7 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void DMA1_Stream0_IRQHandler(void);
 void DMA1_Stream5_IRQHandler(void);
+void SPI3_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
