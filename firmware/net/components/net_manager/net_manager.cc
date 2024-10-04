b111acdaf219d8df410b532050156a3f98825a2e
Brett Regnier <bretternestregnier@gmail.com>
2024-10-04 16:44:52 -0600

    Updating libquicr to newest

--- firmware/net/components/net_manager/net_manager.cc
+++ firmware/net/components/net_manager/net_manager.cc
@@ -6,7 +6,6 @@
 #include "serial_packet.hh"
 #include "qchat.hh"
 
-#include <transport/transport.h>
 #include "esp_log.h"
 
 #include <cstdint>
@@ -20,16 +19,17 @@ typedef struct
 } WatchRoomParams;
 
 NetManager::NetManager(SerialPacketManager& ui_layer,
-    Wifi& wifi,
-    std::shared_ptr<QSession> qsession,
-    std::shared_ptr<AsyncQueue<QuicrObject>> inbound_objects)
+    Wifi& wifi
+    // std::shared_ptr<QSession> qsession,
+    // std::shared_ptr<AsyncQueue<QuicrObject>> inbound_objects
+    )
     : ui_layer(ui_layer),
-    wifi(wifi),
-    quicr_session(qsession),
-    inbound_objects(inbound_objects)
+    wifi(wifi)
+    // quicr_session(qsession),
+    // inbound_objects(inbound_objects)
 {
     xTaskCreate(HandleSerial, "handle_serial_task", 8192 * 2, (void*)this, 13, NULL);
-    xTaskCreate(HandleNetwork, "handle_network", 4096, (void*)this, 13, NULL);
+    // xTaskCreate(HandleNetwork, "handle_network", 4096, (void*)this, 13, NULL);
 }
 
 void NetManager::Update()
@@ -72,7 +72,7 @@ void NetManager::HandleSerial(void* param)
 
                 // Skip the packetId and go to the next part of the packet data
                 uint8_t sub_message_type = rx_packet->GetData<uint8_t>(5, 1);
-                self->HandleQChatMessages(sub_message_type, rx_packet, 6);
+                // self->HandleQChatMessages(sub_message_type, rx_packet, 6);
             }
 
             rx_packets.pop_front();
@@ -80,253 +80,253 @@ void NetManager::HandleSerial(void* param)
     }
 }
 
-void NetManager::HandleQChatMessages(uint8_t message_type,
-    const std::unique_ptr<SerialPacket>& rx_packet,
-    const size_t offset)
-{
-    if (rx_packet == nullptr)
-    {
-        return;
-    }
+// void NetManager::HandleQChatMessages(uint8_t message_type,
+//     const std::unique_ptr<SerialPacket>& rx_packet,
+//     const size_t offset)
+// {
+//     if (rx_packet == nullptr)
+//     {
+//         return;
+//     }
 
-    if (quicr_session == nullptr)
-    {
-        Logger::Log(Logger::Level::Error, "Quicr session is null");
-        return;
-    }
+//     if (quicr_session == nullptr)
+//     {
+//         Logger::Log(Logger::Level::Error, "Quicr session is null");
+//         return;
+//     }
 
-    if (!wifi.IsConnected())
-    {
-        // Not connected to wifi just skip
+//     if (!wifi.IsConnected())
+//     {
+//         // Not connected to wifi just skip
 
-        // Reject, and instead we need to reply with a NACK of sorts
-        return;
-    }
+//         // Reject, and instead we need to reply with a NACK of sorts
+//         return;
+//     }
 
-    qchat::MessageTypes msg_type = static_cast<qchat::MessageTypes>(message_type);
+//     qchat::MessageTypes msg_type = static_cast<qchat::MessageTypes>(message_type);
 
-    switch (msg_type)
-    {
-        case qchat::MessageTypes::Watch:
-        {
-            Logger::Log(Logger::Level::Info, "Got an watch message");
+//     switch (msg_type)
+//     {
+//         case qchat::MessageTypes::Watch:
+//         {
+//             Logger::Log(Logger::Level::Info, "Got an watch message");
 
-            qchat::WatchRoom* watch = new qchat::WatchRoom();
-            if (!qchat::Codec::decode(*watch, rx_packet, offset))
-            {
-                Logger::Log(Logger::Level::Info, "NetManager:QChatMessage:Watch Decode  Failed");
-                return;
-            }
+//             qchat::WatchRoom* watch = new qchat::WatchRoom();
+//             if (!qchat::Codec::decode(*watch, rx_packet, offset))
+//             {
+//                 Logger::Log(Logger::Level::Info, "NetManager:QChatMessage:Watch Decode  Failed");
+//                 return;
+//             }
 
-            WatchRoomParams* watch_params = new WatchRoomParams();
-            watch_params->watch = watch;
-            watch_params->manager = this;
+//             WatchRoomParams* watch_params = new WatchRoomParams();
+//             watch_params->watch = watch;
+//             watch_params->manager = this;
 
-            xTaskCreate(HandleWatchMessage,
-                "handle_watch_message",
-                16384,
-                (void*)watch_params,
-                13,
-                NULL);
+//             xTaskCreate(HandleWatchMessage,
+//                 "handle_watch_message",
+//                 16384,
+//                 (void*)watch_params,
+//                 13,
+//                 NULL);
 
-            break;
-        }
-        case qchat::MessageTypes::Ascii:
-        {
-            Logger::Log(Logger::Level::Info, "Got an ascii message");
+//             break;
+//         }
+//         case qchat::MessageTypes::Ascii:
+//         {
+//             Logger::Log(Logger::Level::Info, "Got an ascii message");
 
-            qchat::Ascii ascii;
-            ascii.message.push_back((uint8_t)qchat::MessageTypes::Ascii);
-            bool result = qchat::Codec::decode(ascii, rx_packet, offset);
-            Logger::Log(Logger::Level::Debug, "Decoded an ascii message");
+//             qchat::Ascii ascii;
+//             ascii.message.push_back((uint8_t)qchat::MessageTypes::Ascii);
+//             bool result = qchat::Codec::decode(ascii, rx_packet, offset);
+//             Logger::Log(Logger::Level::Debug, "Decoded an ascii message");
 
-            if (!result)
-            {
-                Logger::Log(Logger::Level::Error, "NetManager:QChatMessage: Decode Ascii Message Failed");
-                return;
-            }
+//             if (!result)
+//             {
+//                 Logger::Log(Logger::Level::Error, "NetManager:QChatMessage: Decode Ascii Message Failed");
+//                 return;
+//             }
 
-            auto nspace = quicr_session->to_namespace(ascii.message_uri);
-            // quicr::Namespace nspace(0xA11CEE00000001010007000000000000_name, 80);
+//             auto nspace = quicr_session->to_namespace(ascii.message_uri);
+//             // quicr::Namespace nspace(0xA11CEE00000001010007000000000000_name, 80);
 
-            auto bytes = qchat::Codec::string_to_bytes(ascii.message);
+//             auto bytes = qchat::Codec::string_to_bytes(ascii.message);
 
-            Logger::Log(Logger::Level::Info, "Send to quicr session to publish");
-            quicr_session->publish(nspace, bytes);
-            break;
-        }
-        case qchat::MessageTypes::Audio:
-        {
+//             Logger::Log(Logger::Level::Info, "Send to quicr session to publish");
+//             quicr_session->publish(nspace, bytes);
+//             break;
+//         }
+//         case qchat::MessageTypes::Audio:
+//         {
 
-            Logger::Log(Logger::Level::Info, "Got an audio message");
+//             Logger::Log(Logger::Level::Info, "Got an audio message");
 
-            // Get the room len
-            uint32_t room_len = rx_packet->GetData<uint32_t>(offset, 4);
-            uint32_t off = offset + 4;
+//             // Get the room len
+//             uint32_t room_len = rx_packet->GetData<uint32_t>(offset, 4);
+//             uint32_t off = offset + 4;
 
-            // Get the room uri
-            std::string room_uri;
-            for (int i =0; i < room_len; ++i)
-            {
-                room_uri.push_back(rx_packet->GetData<char>(off, 1));
-                off += 1;
-            }
+//             // Get the room uri
+//             std::string room_uri;
+//             for (int i =0; i < room_len; ++i)
+//             {
+//                 room_uri.push_back(rx_packet->GetData<char>(off, 1));
+//                 off += 1;
+//             }
 
-            // Get the audio len
-            uint16_t audio_len = rx_packet->GetData<uint16_t>(off, 2);
-            off += 2;
+//             // Get the audio len
+//             uint16_t audio_len = rx_packet->GetData<uint16_t>(off, 2);
+//             off += 2;
 
-            std::vector<uint8_t> msg;
-            msg.reserve(audio_len + 1); // +1 for type
-            msg.push_back((uint8_t)qchat::MessageTypes::Audio);
-            Logger::Log(Logger::Level::Info, "Audio len ", audio_len);
-            for (uint16_t i = 0; i < audio_len; ++i)
-            {
-                msg.push_back(rx_packet->GetData<uint8_t>(off, 1));
-                off+=1;
-            }
+//             std::vector<uint8_t> msg;
+//             msg.reserve(audio_len + 1); // +1 for type
+//             msg.push_back((uint8_t)qchat::MessageTypes::Audio);
+//             Logger::Log(Logger::Level::Info, "Audio len ", audio_len);
+//             for (uint16_t i = 0; i < audio_len; ++i)
+//             {
+//                 msg.push_back(rx_packet->GetData<uint8_t>(off, 1));
+//                 off+=1;
+//             }
 
-            auto nspace = quicr_session->to_namespace(room_uri);
+//             auto nspace = quicr_session->to_namespace(room_uri);
 
-            Logger::Log(Logger::Level::Info, "Send audio to quicr session to publish");
-            quicr_session->publish(nspace, msg);
-            break;
-        }
-        default:
-        {
-            Logger::Log(Logger::Level::Warn, "NetManager:QChatMessage: Unknown Message");
-            break;
-        }
-    }
-}
+//             Logger::Log(Logger::Level::Info, "Send audio to quicr session to publish");
+//             quicr_session->publish(nspace, msg);
+//             break;
+//         }
+//         default:
+//         {
+//             Logger::Log(Logger::Level::Warn, "NetManager:QChatMessage: Unknown Message");
+//             break;
+//         }
+//     }
+// }
 
-void NetManager::HandleWatchMessage(void* params)
-{
-    WatchRoomParams* watch_room_params = (WatchRoomParams*)params;
-    NetManager* self = watch_room_params->manager;
-    qchat::WatchRoom* watch = watch_room_params->watch;
+// void NetManager::HandleWatchMessage(void* params)
+// {
+//     WatchRoomParams* watch_room_params = (WatchRoomParams*)params;
+//     NetManager* self = watch_room_params->manager;
+//     qchat::WatchRoom* watch = watch_room_params->watch;
 
-    Logger::Log(Logger::Level::Info, "Room URI: ", watch->room_uri.c_str());
-    Logger::Log(Logger::Level::Info, "Publisher URI: ", watch->publisher_uri.c_str());
-    try
-    {
-        if (!self->quicr_session->publish_intent(self->quicr_session->to_namespace(watch->room_uri)))
-        {
-            Logger::Log(Logger::Level::Error, "NetManager: QChatMessage:Watch publish_intent error");
-            return;
-        }
+//     Logger::Log(Logger::Level::Info, "Room URI: ", watch->room_uri.c_str());
+//     Logger::Log(Logger::Level::Info, "Publisher URI: ", watch->publisher_uri.c_str());
+//     try
+//     {
+//         if (!self->quicr_session->publish_intent(self->quicr_session->to_namespace(watch->room_uri)))
+//         {
+//             Logger::Log(Logger::Level::Error, "NetManager: QChatMessage:Watch publish_intent error");
+//             return;
+//         }
 
-        Logger::Log(Logger::Level::Debug, "Publish Intended");
+//         Logger::Log(Logger::Level::Debug, "Publish Intended");
 
-        if (!self->quicr_session->subscribe(self->quicr_session->to_namespace(watch->room_uri)))
-        {
-            Logger::Log(Logger::Level::Error, "NetManager: QChatMessage:Watch subscribe error");
-            return;
-        }
+//         if (!self->quicr_session->subscribe(self->quicr_session->to_namespace(watch->room_uri)))
+//         {
+//             Logger::Log(Logger::Level::Error, "NetManager: QChatMessage:Watch subscribe error");
+//             return;
+//         }
 
-        Logger::Log(Logger::Level::Debug, "Subscribed");
+//         Logger::Log(Logger::Level::Debug, "Subscribed");
 
-        // why is this here?
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
+//         // why is this here?
+//         vTaskDelay(2000 / portTICK_PERIOD_MS);
 
-        auto watch_ok_packet = std::make_unique<SerialPacket>(xTaskGetTickCount());
-        watch_ok_packet->SetData(SerialPacket::Types::QMessage, 0, 1);
-        watch_ok_packet->SetData(self->ui_layer.NextPacketId(), 1, 2);
-        watch_ok_packet->SetData(1, 3, 2);
-        watch_ok_packet->SetData(qchat::MessageTypes::WatchOk, 5, 1);
+//         auto watch_ok_packet = std::make_unique<SerialPacket>(xTaskGetTickCount());
+//         watch_ok_packet->SetData(SerialPacket::Types::QMessage, 0, 1);
+//         watch_ok_packet->SetData(self->ui_layer.NextPacketId(), 1, 2);
+//         watch_ok_packet->SetData(1, 3, 2);
+//         watch_ok_packet->SetData(qchat::MessageTypes::WatchOk, 5, 1);
 
-        Logger::Log(Logger::Level::Debug, "Sending WatchOk to UI");
-        self->ui_layer.EnqueuePacket(std::move(watch_ok_packet));
-    }
-    catch (std::runtime_error& ex)
-    {
-        Logger::Log(Logger::Level::Error, "Failed to watch: ", ex.what());
-    }
+//         Logger::Log(Logger::Level::Debug, "Sending WatchOk to UI");
+//         self->ui_layer.EnqueuePacket(std::move(watch_ok_packet));
+//     }
+//     catch (std::runtime_error& ex)
+//     {
+//         Logger::Log(Logger::Level::Error, "Failed to watch: ", ex.what());
+//     }
 
-    delete watch_room_params->watch;
-    delete watch_room_params;
+//     delete watch_room_params->watch;
+//     delete watch_room_params;
 
-    vTaskDelete(NULL);
-}
+//     vTaskDelete(NULL);
+// }
 
-void NetManager::HandleNetwork(void* param)
-{
-    // part 1, parse quicr message
-    // Get the quicr messages from the inbound objects
-    NetManager* self = (NetManager*)param;
-    while (true)
-    {
-        try
-        {
-            // TODO use a conditional variable
-            // Set the delay
-            vTaskDelay(50 / portTICK_PERIOD_MS);
+// void NetManager::HandleNetwork(void* param)
+// {
+//     // part 1, parse quicr message
+//     // Get the quicr messages from the inbound objects
+//     NetManager* self = (NetManager*)param;
+//     while (true)
+//     {
+//         try
+//         {
+//             // TODO use a conditional variable
+//             // Set the delay
+//             vTaskDelay(50 / portTICK_PERIOD_MS);
 
-            if (self->inbound_objects->empty())
-            {
-                continue;
-            }
+//             if (self->inbound_objects->empty())
+//             {
+//                 continue;
+//             }
 
-            const uint16_t Bytes_In_128_Bits = 128 / 8;
+//             const uint16_t Bytes_In_128_Bits = 128 / 8;
 
-            // Get the next item
-            auto qobj = self->inbound_objects->pop();
-            auto qname = qobj.name;
-            auto qdata = qobj.data;
+//             // Get the next item
+//             auto qobj = self->inbound_objects->pop();
+//             auto qname = qobj.name;
+//             auto qdata = qobj.data;
 
-            // Create a net packet
-            std::unique_ptr<SerialPacket> packet = std::make_unique<SerialPacket>();
+//             // Create a net packet
+//             std::unique_ptr<SerialPacket> packet = std::make_unique<SerialPacket>();
 
-            // Set the type
-            packet->SetData(SerialPacket::Types::QMessage, 0, 1);
+//             // Set the type
+//             packet->SetData(SerialPacket::Types::QMessage, 0, 1);
 
-            // Set the id
-            packet->SetData(self->ui_layer.NextPacketId(), 1, 2);
+//             // Set the id
+//             packet->SetData(self->ui_layer.NextPacketId(), 1, 2);
 
-            // Set the length
-            uint16_t total_len = 10 + Bytes_In_128_Bits + qobj.data.size();
-            packet->SetData(total_len, 3, 2);
+//             // Set the length
+//             uint16_t total_len = 10 + Bytes_In_128_Bits + qobj.data.size();
+//             packet->SetData(total_len, 3, 2);
 
-            // Set the message type
-            packet->SetData((int)qchat::MessageTypes::Ascii, 5, 1);
+//             // Set the message type
+//             packet->SetData((int)qchat::MessageTypes::Ascii, 5, 1);
 
-            // Set the length of the ascii message qname
-            packet->SetData(Bytes_In_128_Bits, 6, 4);
+//             // Set the length of the ascii message qname
+//             packet->SetData(Bytes_In_128_Bits, 6, 4);
 
-            // Set the name, which is 128 bits.
-            size_t offset = 10;
-            for (size_t i = 0; i < Bytes_In_128_Bits; ++i)
-            {
-                packet->SetData(qname[i], offset, 1);
-                offset += 1;
-            }
+//             // Set the name, which is 128 bits.
+//             size_t offset = 10;
+//             for (size_t i = 0; i < Bytes_In_128_Bits; ++i)
+//             {
+//                 packet->SetData(qname[i], offset, 1);
+//                 offset += 1;
+//             }
 
-            // Set the length of the data
-            packet->SetData(qdata.size(), offset, 4);
-            offset += 4;
+//             // Set the length of the data
+//             packet->SetData(qdata.size(), offset, 4);
+//             offset += 4;
 
-            // Get the message from the item
-            for (size_t i = 0; i < qdata.size(); ++i)
-            {
-                packet->SetData(qdata[i], offset, 1);
-                offset += 1;
-            }
+//             // Get the message from the item
+//             for (size_t i = 0; i < qdata.size(); ++i)
+//             {
+//                 packet->SetData(qdata[i], offset, 1);
+//                 offset += 1;
+//             }
 
-            // Logger::Log(Logger::Level::Info, "Packet data total len %d", total_len);
-            // for (size_t i = 0; i < total_len; ++i)
-            // {
-            //     Logger::Log(Logger::Level::Info, (int)i, packet->GetData<int>(i, 1));
-            // }
-            // Logger::Log(Logger::Level::Info, "Enqueue serial packet that came from the network");
-            // Enqueue the packet to go to the UI
-            self->ui_layer.EnqueuePacket(std::move(packet));
-        }
-        catch (const std::exception& ex)
-        {
-            Logger::Log(Logger::Level::Info, "[HandleNetworkError]", ex.what());
-        }
-    }
-}
+//             // Logger::Log(Logger::Level::Info, "Packet data total len %d", total_len);
+//             // for (size_t i = 0; i < total_len; ++i)
+//             // {
+//             //     Logger::Log(Logger::Level::Info, (int)i, packet->GetData<int>(i, 1));
+//             // }
+//             // Logger::Log(Logger::Level::Info, "Enqueue serial packet that came from the network");
+//             // Enqueue the packet to go to the UI
+//             self->ui_layer.EnqueuePacket(std::move(packet));
+//         }
+//         catch (const std::exception& ex)
+//         {
+//             Logger::Log(Logger::Level::Info, "[HandleNetworkError]", ex.what());
+//         }
+//     }
+// }
 
 /**                          Private Functions                               **/
 void NetManager::HandleSerialCommands()
--- firmware/net/components/net_manager/qsession.cpp
+++ firmware/net/components/net_manager/qsession.cpp
@@ -1,146 +1,145 @@
-#include<future>
-#include "esp_pthread.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+// #include<future>
+// #include "esp_pthread.h"
+// #include "freertos/FreeRTOS.h"
+// #include "freertos/task.h"
 
-#include <condition_variable>
-#include <future>
-#include <map>
-#include <mutex>
-#include <memory>
-#include <set>
-#include <thread>
+// #include <condition_variable>
+// #include <future>
+// #include <map>
+// #include <mutex>
+// #include <memory>
+// #include <set>
+// #include <thread>
 
-#include "qsession.hh"
-#include "transport/transport.h"
+// #include "qsession.hh"
 
-static const uint16_t default_ttl_ms = 1000;
-
-
-[[maybe_unused]] static esp_pthread_cfg_t create_config(const char* name, int core_id, int stack, int prio)
-{
-  auto cfg = esp_pthread_get_default_config();
-  cfg.thread_name = name;
-  cfg.pin_to_core = core_id;
-  cfg.stack_size = stack;
-  cfg.prio = prio;
-  return cfg;
-}
+// static const uint16_t default_ttl_ms = 1000;
 
 
-QSession::QSession(quicr::RelayInfo relay,
-  std::shared_ptr<AsyncQueue<QuicrObject>> app_queue) :
-  inbound_objects(app_queue)
-{
-  logger = std::make_shared<cantina::Logger>("qsession_logger");
-  add_uri_templates();
-  logger->Log("Connecting to " + relay.hostname + ":" +
-    std::to_string(relay.port));
-  qtransport::TransportConfig tcfg{ .tls_cert_filename = NULL,
-                                    .tls_key_filename = NULL };
-  client = std::make_unique<quicr::Client>(relay, tcfg, logger);
-
-}
-
-quicr::Namespace QSession::to_namespace(const std::string& ns_str)
-{
-  return url_encoder.EncodeUrl(ns_str);
-}
-
-void QSession::set_app_queue(std::shared_ptr<AsyncQueue<QuicrObject>> q)
-{
-  inbound_objects = q;
-}
-
-bool
-QSession::connect()
-{
-  // Connect to the quicr relay
-  if (!client->connect())
-  {
-    return false;
-  }
-  return true;
-}
-
-bool
-QSession::subscribe(quicr::Namespace ns)
-{
-  if (sub_delegates.count(ns))
-  {
-    return true;
-  }
+// [[maybe_unused]] static esp_pthread_cfg_t create_config(const char* name, int core_id, int stack, int prio)
+// {
+//   auto cfg = esp_pthread_get_default_config();
+//   cfg.thread_name = name;
+//   cfg.pin_to_core = core_id;
+//   cfg.stack_size = stack;
+//   cfg.prio = prio;
+//   return cfg;
+// }
 
 
-  auto promise = std::promise<bool>();
-  auto future = promise.get_future();
-  const auto delegate =
-    std::make_shared<SubDelegate>(logger, inbound_objects, std::move(promise));
+// QSession::QSession(quicr::RelayInfo relay,
+//   std::shared_ptr<AsyncQueue<QuicrObject>> app_queue) :
+//   inbound_objects(app_queue)
+// {
+//   logger = std::make_shared<cantina::Logger>("qsession_logger");
+//   add_uri_templates();
+//   logger->Log("Connecting to " + relay.hostname + ":" +
+//     std::to_string(relay.port));
+//   qtransport::TransportConfig tcfg{ .tls_cert_filename = NULL,
+//                                     .tls_key_filename = NULL };
+//   client = std::make_unique<quicr::Client>(relay, tcfg, logger);
 
-  logger->Log("Subscribe to " + std::string(ns));
-  quicr::bytes empty{};
-  client->subscribe(delegate,
-    ns,
-    quicr::SubscribeIntent::immediate,
-    "bogus_origin_url",
-    false,
-    "bogus_auth_token",
-    std::move(empty));
+// }
 
-  const auto success = future.get();
-  if (success)
-  {
-    sub_delegates.insert_or_assign(ns, delegate);
-  }
+// quicr::Namespace QSession::to_namespace(const std::string& ns_str)
+// {
+//   return url_encoder.EncodeUrl(ns_str);
+// }
 
-  return success;
+// void QSession::set_app_queue(std::shared_ptr<AsyncQueue<QuicrObject>> q)
+// {
+//   inbound_objects = q;
+// }
 
-}
+// bool
+// QSession::connect()
+// {
+//   // Connect to the quicr relay
+//   if (!client->connect())
+//   {
+//     return false;
+//   }
+//   return true;
+// }
 
-bool
-QSession::publish_intent(quicr::Namespace ns)
-{
-  logger->Log("Publish Intent for namespace: " + std::string(ns));
-  auto promise = std::promise<bool>();
-  auto future = promise.get_future();
-  const auto delegate =
-    std::make_shared<PubDelegate>(logger, std::move(promise));
-
-  client->publishIntent(delegate, ns, {}, {}, {});
-  return true;//future.get();
-}
-
-void QSession::unsubscribe(quicr::Namespace nspace) {}
-
-void
-QSession::publish(const quicr::Name& name, quicr::bytes& data)
-{
-  // logger->Log("Publish, name=" + std::string(name) +
-  //             " size=" + std::to_string(data.size()));
-  client->publishNamedObject(name, 0, default_ttl_ms, false, std::move(data));
-}
+// bool
+// QSession::subscribe(quicr::Namespace ns)
+// {
+//   if (sub_delegates.count(ns))
+//   {
+//     return true;
+//   }
 
 
+//   auto promise = std::promise<bool>();
+//   auto future = promise.get_future();
+//   const auto delegate =
+//     std::make_shared<SubDelegate>(logger, inbound_objects, std::move(promise));
 
-//
-// Private Implementation
-//
+//   logger->Log("Subscribe to " + std::string(ns));
+//   quicr::bytes empty{};
+//   client->subscribe(delegate,
+//     ns,
+//     quicr::SubscribeIntent::immediate,
+//     "bogus_origin_url",
+//     false,
+//     "bogus_auth_token",
+//     std::move(empty));
 
-void QSession::add_uri_templates()
-{
+//   const auto success = future.get();
+//   if (success)
+//   {
+//     sub_delegates.insert_or_assign(ns, delegate);
+//   }
 
-  // setup namespace encoder templates
-  // TODO (this should come config file passed from ui to net chip during boot)
-  // TODO harcoding for now
-  static std::set<std::string> url_templates{
-    // "quicr://webex.cisco.com<pen=1>/version/<int8>/appId/<int8>/org/<int12>/channel/<int16>/room/<int16>/endpoint/<int16>",
-    "quicr://webex.cisco.com<pen=10>/version/<int8>/appId/<int8>/org/<int12>/channel/<int16>/room/<int16>",
-  };
+//   return success;
 
-  for (const std::string& url_template : url_templates)
-  {
-    logger->Log("Add URL templates for " + url_template);
-    url_encoder.AddTemplate(url_template, true);
-  }
+// }
 
-}
+// bool
+// QSession::publish_intent(quicr::Namespace ns)
+// {
+//   logger->Log("Publish Intent for namespace: " + std::string(ns));
+//   auto promise = std::promise<bool>();
+//   auto future = promise.get_future();
+//   const auto delegate =
+//     std::make_shared<PubDelegate>(logger, std::move(promise));
+
+//   client->publishIntent(delegate, ns, {}, {}, {});
+//   return true;//future.get();
+// }
+
+// void QSession::unsubscribe(quicr::Namespace nspace) {}
+
+// void
+// QSession::publish(const quicr::Name& name, quicr::bytes& data)
+// {
+//   // logger->Log("Publish, name=" + std::string(name) +
+//   //             " size=" + std::to_string(data.size()));
+//   client->publishNamedObject(name, 0, default_ttl_ms, false, std::move(data));
+// }
+
+
+
+// //
+// // Private Implementation
+// //
+
+// void QSession::add_uri_templates()
+// {
+
+//   // setup namespace encoder templates
+//   // TODO (this should come config file passed from ui to net chip during boot)
+//   // TODO harcoding for now
+//   static std::set<std::string> url_templates{
+//     // "quicr://webex.cisco.com<pen=1>/version/<int8>/appId/<int8>/org/<int12>/channel/<int16>/room/<int16>/endpoint/<int16>",
+//     "quicr://webex.cisco.com<pen=10>/version/<int8>/appId/<int8>/org/<int12>/channel/<int16>/room/<int16>",
+//   };
+
+//   for (const std::string& url_template : url_templates)
+//   {
+//     logger->Log("Add URL templates for " + url_template);
+//     url_encoder.AddTemplate(url_template, true);
+//   }
+
+// }
