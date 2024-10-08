22ff7ed55b1a8fe75d793ea1337f6a3b166ccdfe
Tim Evens <tievens@cisco.com>
2024-10-08 13:22:55 -0700

    Update libquicr and add netinet/udp.h

---
+++ firmware/net/include/netinet/udp.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2015-2016 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2020 Francesco Giancane <francesco.giancane@accenture.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _NETINET_UDP_H
+#define _NETINET_UDP_H
+
+#include "lwip/tcp.h"
+
+#endif /* _NETINET_UDP_H  */
