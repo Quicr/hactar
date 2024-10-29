8311ca675d3c35ba80acadf4552a4cf97b914c2a
Richard Barnes <rlb@ipv.sx>
2024-10-29 08:57:33 -0400

    Change ownership model to avoid ownership conflicts

--- firmware/ui-rs/hactar-platform/src/lib.rs
+++ firmware/ui-rs/hactar-platform/src/lib.rs
@@ -8,6 +8,16 @@
 
 /// The [`Platform`] trait encapsulates the capabilities that we expect to be exposed by a device
 /// that supports the Hactar UI application.
+///
+/// A Platform object starts as a collection of capabilities -- LEDs, sound processing, etc.  These
+/// capabilities can then be taken by calling code for use in the application, using the `take_*`
+/// methods.
+///
+/// XXX(RLB): It would also be possible to have the platform retain ownership of the capabilities,
+/// and just hand references to the calling code.  On the one hand, this would avoid all the
+/// messing around with `Option` to indicates whether a capability has already been claimed.  On
+/// the other hand, this would make it difficult to have multiple independent tasks holding
+/// different capabilities, since they would all have to hold references to the platform.
 pub trait Platform: 'static {
     /// A human-readable name for the platform
     fn name() -> &'static str;
@@ -16,7 +26,7 @@ pub trait Platform: 'static {
     type StatusLed<'d>: Led;
 
     /// An LED that can be used as a status indicator
-    fn status_led<'d>(&'d mut self) -> Self::StatusLed<'d>;
+    fn status_led<'d>(&mut self) -> Option<Self::StatusLed<'d>>;
 
     /// Waits until `millis` milliseconds have elapsed
     async fn after_millis(millis: u64);
