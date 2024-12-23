2fc6aec510879f4baf653c5f68b85ae9bb5f9910
Brett Regnier <bretternestregnier@gmail.com>
2024-12-23 09:38:53 -0700

    Moved changes to net into a different folder, restored original net folder

--- firmware/net/core/inc/net.hh
+++ firmware/net/core/inc/net.hh
@@ -1,6 +1,3 @@
-#ifndef NET_HH
-#define NET_HH
-
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -11,21 +8,25 @@
 #include "esp_event.h"
 
 #include "logger.hh"
-// #include "serial_esp.hh"
-// #include "serial_packet_manager.hh"
-// #include "net_manager.hh"
+#include "serial_esp.hh"
+#include "serial_packet_manager.hh"
+#include "net_manager.hh"
 
-// #include "wifi.hh"
+#include "wifi.hh"
 
-// #include "qsession.hh"
+#include "qsession.hh"
 
-// static NetManager* manager = nullptr;
-// static SerialEsp* ui_uart1 = nullptr;
-// static SerialPacketManager* ui_layer = nullptr;
-// static Wifi* wifi = nullptr;
-// static std::shared_ptr<QSession> qsession = nullptr;
-// static std::shared_ptr<AsyncQueue<QuicrObject>> inbound_queue;
+static NetManager* manager = nullptr;
+static SerialEsp* ui_uart1 = nullptr;
+static SerialPacketManager* ui_layer = nullptr;
+static SerialPacketManager* audio_layer = nullptr;
+static Wifi* wifi = nullptr;
+static std::shared_ptr<QSession> qsession = nullptr;
+static std::shared_ptr<AsyncQueue<QuicrObject>> inbound_queue;
 
 static bool qsession_connected = false;
 
-#endif
+static void SetupPins();
+static void SetupComponents();
+
+
--- firmware/net/core/src/net.cc
+++ firmware/net/core/src/net.cc
@@ -11,117 +11,24 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
-#include "driver/ledc.h"
 #include "driver/uart.h"
 #include "nvs_flash.h"
 #include "esp_event.h"
 
-#include "peripherals.hh"
-#include "serial.hh"
+#include "serial_esp.hh"
+#include "serial_packet_manager.hh"
+#include "net_manager.hh"
+
 #include "wifi.hh"
+#include "net_pins.hh"
 
-#include "lwip/sockets.h"
-#include "lwip/netdb.h"
-
-#define TEST_SERVER_IP "192.168.50.20"
-#define TEST_SERVER_PORT 12345
-
-// TODO tcp client should now also receive packets and push them to serial
-
-Serial* ui_layer;
-
-static void TCPClientTask(void* params)
-{
-    static const char* tcp_tag = "TCP Client Task";
-    uint32_t packets_sent = 0;
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
-    {
-        ESP_LOGE(tcp_tag, "Unable to create socket");
-        vTaskDelete(NULL);
-    }
-
-    struct sockaddr_in dest_addr;
-    dest_addr.sin_addr.s_addr = inet_addr(TEST_SERVER_IP);
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port = htons(TEST_SERVER_PORT);
-
-
-    // Connect to server
-    int err = connect(sock, (struct sockaddr*)&dest_addr, sizeof(dest_addr));
-    if (err < 0)
-    {
-        ESP_LOGE(tcp_tag, "Socket unable to connect errno %d", err);
-        vTaskDelete(NULL);
-    }
-    ESP_LOGI(tcp_tag, "Connected to server");
-
-    Serial::packet_t* packet = nullptr;
-    while (true)
-    {
-        vTaskDelay(10/portTICK_PERIOD_MS);
-        // Get the data from our serial
-        packet = ui_layer->GetReadyRxPacket();
-        if (packet == nullptr)
-        {
-            continue;
-        }
-        
-        err = send(sock, packet->data, packet->length, 0);
-        if (err < 0)
-        {
-            ESP_LOGE(tcp_tag, "Error sending data: errno %d", err);
-        }
-        else
-        {
-            ++packets_sent;
-            ESP_LOGI(tcp_tag, "Data sent %lu", packets_sent);
-        }
-    }
-    close(sock);
-    ESP_LOGI(tcp_tag, "socket closed");
-    vTaskDelete(NULL);
-}
-
+#include "qsession.hh"
+#include "logger.hh"
 
 extern "C" void app_main(void)
 {
-    QueueHandle_t uart_queue;
-    InitializeGPIO();
-    IntitializeLEDs();
-    // Not using for now
-    // IntitializePWM();
-
-
-    // Configure the uart
-    uart_config_t uart1_config = {
-        .baud_rate = 921600,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_EVEN,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .rx_flow_ctrl_thresh = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_DEFAULT // UART_SCLK_DEFAULT
-    };
-
-    InitializeQueuedUART(uart1_config, UART1, uart_queue,
-        RX_BUFF_SIZE, TX_BUFF_SIZE,
-        EVENT_QUEUE_SIZE, TX_PIN, RX_PIN,
-        RTS_PIN, CTS_PIN, ESP_INTR_FLAG_LOWMED);
-
-
-    ui_layer = new Serial(UART1, uart_queue, 4096 * 2, 4096 * 2, 30, 30);
-    Wifi wifi;
-    wifi.Connect("Regnier", "Spikeball21Computers");
-
-    while (!wifi.IsConnected())
-    {
-        ESP_LOGW("net.cc", "REMOVE ME - Waiting to connect to wifi");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    // TODO we need to wait until the tcp client is also connected before we say we can take 
-    // packets
-    xTaskCreate(TCPClientTask, "tcp_client_task", 4096, NULL, 5, NULL);
+    SetupPins();
+    SetupComponents();
 
     gpio_set_level(NET_LED_R, 1);
     gpio_set_level(NET_LED_G, 1);
@@ -130,54 +37,110 @@ extern "C" void app_main(void)
     gpio_set_level(NET_DEBUG_2, 0);
     gpio_set_level(NET_DEBUG_3, 0);
 
+    int next = 0;
+    gpio_set_level(NET_LED_R, 0);
+
     // Ready for normal operations
     gpio_set_level(NET_STAT, 1);
-
 
     // This is the lazy way of doing it, otherwise we should use a esp_timer.
     uint32_t blink_cnt = 0;
     while (1)
     {
+        if (blink_cnt++ == 100)
+        {
+            gpio_set_level(NET_LED_R, next);
+            next = next ? 0 : 1;
+            blink_cnt = 0;
+        }
 
+        manager->Update();
 
-        // manager->Update();
+        auto state = wifi->GetState();
+        if (state == Wifi::State::Connected && !qsession_connected)
+        {
+            Logger::Log(Logger::Level::Info, "Net app_main Connecting to QSession");
+            qsession->connect();
+            qsession_connected = true;
+        }
 
-        // auto state = wifi->GetState();
-        // if (state == Wifi::State::Connected && !qsession_connected)
-        // {
-        //     Logger::Log(Logger::Level::Info, "Net app_main Connecting to QSession");
-        //     qsession->connect();
-        //     qsession_connected = true;
-        // }
-
-        vTaskDelay(1600 / portTICK_PERIOD_MS);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
+}
+
+static void SetupPins()
+{
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = NET_STAT_MASK;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_config(&io_conf);
+
+    gpio_set_level(NET_STAT, 0);
+
+    // LED init
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = LED_MASK;
+    io_conf.pull_down_en = (gpio_pulldown_t)0;
+    io_conf.pull_up_en = (gpio_pullup_t)0;
+    gpio_config(&io_conf);
+
+    // Debug init
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = NET_DEBUG_MASK;
+    io_conf.pull_down_en = (gpio_pulldown_t)0;
+    io_conf.pull_up_en = (gpio_pullup_t)0;
+    gpio_config(&io_conf);
+
+    // Configure the uart
+    uart_config_t uart1_config = {
+        .baud_rate = 921600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_EVEN,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .rx_flow_ctrl_thresh = UART_HW_FLOWCTRL_MAX,
+        .source_clk = UART_SCLK_DEFAULT // UART_SCLK_DEFAULT
+    };
+
+    // uart_intr_config_t uart1_intr = {
+    //     .intr_enable_mask
+    // }
+
+    // Setup serial interface for ui
+    ui_uart1 = new SerialEsp(UART1, 17, 18, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, uart1_config, 4096);
+
+    Logger::Log(Logger::Level::Info, "Pin setup complete");
 }
 
 void SetupComponents()
 {
+    // UART to the ui
+    ui_layer = new SerialPacketManager(ui_uart1);
+    wifi = new Wifi(*ui_layer);
 
-
-    // wifi = new Wifi(*ui_layer);
-
-    // inbound_queue = std::make_shared<AsyncQueue<QuicrObject>>();
+    inbound_queue = std::make_shared<AsyncQueue<QuicrObject>>();
     char default_relay [] = "192.168.50.20";
     auto relay_name = default_relay;
     uint16_t port = 1234;
-    // quicr::RelayInfo relay{
-    //     .hostname = relay_name,`````````
-    //     .port = port,
-    //     .proto = quicr::RelayInfo::Protocol::UDP
-    // };
-    // qsession = std::make_shared<QSession>(relay, inbound_queue);
+    quicr::RelayInfo relay{
+        .hostname = relay_name,
+        .port = port,
+        .proto = quicr::RelayInfo::Protocol::UDP
+    };
+    qsession = std::make_shared<QSession>(relay, inbound_queue);
 
-    // manager = new NetManager(*ui_layer, *wifi, qsession, inbound_queue);
+    manager = new NetManager(*ui_layer, *wifi, qsession, inbound_queue);
 
     // Wait for wifi to finish initializing
-    // while (!wifi->IsInitialized())
-    // {
-    //     vTaskDelay(10 / portTICK_PERIOD_MS);
-    // }
+    while (!wifi->IsInitialized())
+    {
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
 
     Logger::Log(Logger::Level::Info, "Components ready");
 }
