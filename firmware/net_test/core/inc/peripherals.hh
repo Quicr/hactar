147f0abaa7b4f770fe4a2074e98c64c21c31ecd6
Brett Regnier <bretternestregnier@gmail.com>
2025-01-23 16:24:47 -0700

    feat(serial): Added read isr

--- firmware/net_test/core/inc/peripherals.hh
+++ firmware/net_test/core/inc/peripherals.hh
@@ -5,12 +5,16 @@
 #include "driver/ledc.h"
 #include "driver/uart.h"
 
+#include "serial.hh"
+
 // Pin and peripheral definitions
 #define NET_DEBUG_1         GPIO_NUM_7
 #define NET_DEBUG_2         GPIO_NUM_15
 #define NET_DEBUG_3         GPIO_NUM_16
 #define UI_RX2_NET          GPIO_NUM_17
 #define UI_TX2_NET          GPIO_NUM_18
+#define UI_UART2_CTS_PIN    UART_PIN_NO_CHANGE
+#define UI_UART2_RTS_PIN    UART_PIN_NO_CHANGE
 #define NET_STAT            GPIO_NUM_21
 #define NET_LED_B           GPIO_NUM_36
 #define NET_LED_G           GPIO_NUM_37
@@ -20,20 +24,11 @@
 #define NET_STAT_MASK       1ULL << NET_STAT
 #define NET_DEBUG_MASK      1ULL << NET_DEBUG_1 | 1ULL << NET_DEBUG_2 | 1ULL << NET_DEBUG_3
 
-#define UART1 UART_NUM_1
-#define TX_PIN GPIO_NUM_17
-#define RX_PIN GPIO_NUM_18
-#define CTS_PIN UART_PIN_NO_CHANGE
-#define RTS_PIN UART_PIN_NO_CHANGE
-#define TX_BUFF_SIZE 1024
-#define TX_BUFF_SIZE_2 TX_BUFF_SIZE / 2
-#define RX_BUFF_SIZE 1024
-#define EVENT_QUEUE_SIZE 20
 
 void InitializeGPIO();
 void IntitializeLEDs();
 void IntitializePWM();
-void InitializeQueuedUART(const uart_config_t& uart_config,
+Serial* InitializeQueuedUART(const uart_config_t& uart_config,
     const uart_port_t& uart_port,
     QueueHandle_t& uart_queue,
     const int rx_buff_size,
@@ -43,7 +38,11 @@ void InitializeQueuedUART(const uart_config_t& uart_config,
     const int rx_pin,
     const int rts_pin,
     const int cts_pin,
-    const long int intr_priority
+    const long int intr_priority,
+    const uint16_t serial_tx_task_sz,
+    const uint16_t serial_rx_task_sz,
+    const uint16_t serial_ring_tx_num,
+    const uint16_t serial_ring_rx_num
 );
 
 #endif
