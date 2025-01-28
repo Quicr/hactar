4cbe8c8502d1c19e1bca01f196eb5dc54100fc99
Brett Regnier <bretternestregnier@gmail.com>
2025-01-28 13:08:40 -0700

    feat(serial): update net serial to use isr instead of events

--- firmware/net/components/serial/serial.hh
+++ firmware/net/components/serial/serial.hh
@@ -3,6 +3,8 @@
 #include "driver/uart.h"
 #include "freertos/queue.h"
 #include "esp_event.h"
+#include "hal/uart_ll.h"
+#include "soc/interrupts.h"
 
 #include "ring_buffer.hh"
 
@@ -12,27 +14,42 @@ class Serial
 {
 public:
 
-    Serial(const uart_port_t uart, QueueHandle_t& queue,
-        const size_t tx_task, const size_t rx_task,
-        const size_t tx_rings, const size_t rx_rings);
+    Serial(const uart_port_t port, uart_dev_t& uart, const periph_interrput_t intr_source,
+        const uart_config_t uart_config, const int tx_pin, const int rx_pin,
+        const int rts_pin, const int cts_pin,
+        const uint32_t rx_buff_sz, const uint32_t tx_buff_sz,
+        const uint32_t tx_rings, const uint32_t rx_rings);
+
+    ~Serial();
 
     uint16_t NumReadyRxPackets();
     link_packet_t* Read();
     link_packet_t* Write();
+    void Write(const link_packet_t* packet);
+    void Write(const uint8_t* data, const size_t size);
 
 private:
-    static void WriteTask(void* param);
-    static void ReadTask(void* param);
+    static IRAM_ATTR void Transmit(Serial* self);
+    static IRAM_ATTR void ISRHandler(void* args);
 
-    void HandleRxEvent(uart_event_t& event);
-    void HandleRxData();
-
-    uart_port_t uart;
-    QueueHandle_t& queue;
-
+    uart_port_t port;
+    uart_dev_t& uart;
+    const uint32_t rx_buff_sz;
+    uint8_t* rx_buff;
+    const uint32_t tx_buff_sz;
+    uint8_t* tx_buff;
     RingBuffer<link_packet_t> tx_packets;
     RingBuffer<link_packet_t> rx_packets;
 
-    uint32_t num_recv;
-    uint32_t num_sent;
+    uint32_t rx_buff_write;
+    uint32_t rx_buff_read;
+    uint32_t unread;
+
+    uint32_t tx_buff_write;
+    uint32_t tx_buff_read;
+    uint32_t untransmitted;
+    int32_t num_transmitting;
+    bool tx_free;
+
+    uart_isr_handle_t isr_handle;
 };
