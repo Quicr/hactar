41c932db1f9b875d916be83b0fc63290cae96358
Brett Regnier <bretternestregnier@gmail.com>
2025-01-29 17:01:22 -0700

    feat(ui serial): Added a large transmit buffer and self transmitting code

--- firmware/ui/inc/serial.hh
+++ firmware/ui/inc/serial.hh
@@ -5,11 +5,16 @@
 #include "ring_buffer.hh"
 #include "link_packet_t.hh"
 
+// TODO update write code to push the data into a large
+// buffer, and then use the interrupt to trigger another send.
+
 class Serial
 {
 public:
 
-    Serial(UART_HandleTypeDef* uart, const uint16_t num_rx_packets);
+    Serial(UART_HandleTypeDef* uart, const uint16_t num_rx_packets,
+        uint8_t& tx_buff, const uint32_t tx_buff_sz,
+        uint8_t& rx_buff, const uint32_t rx_buff_sz);
     ~Serial();
 
     void StartReceive();
@@ -17,27 +22,32 @@ public:
 
     link_packet_t* Read();
     void Write(const uint8_t data);
-    void Write(const uint8_t* data, const size_t size);
     void Write(const link_packet_t& packet);
-    size_t Unread();
+    void Write(const uint8_t* data, const uint16_t size);
+    uint16_t Unread();
 
-    void UpdateReadHead(size_t amt);
-    void RxEvent(const uint16_t idx);
-    void TxEvent();
-    bool IsFree();
-    void Free();
+    const UART_HandleTypeDef* UART();
 
+    static void RxISR(Serial* self, const uint16_t idx);
+    static void TxISR(Serial* self);
 protected:
-    void ChangeFreeState();
-
-    static constexpr size_t Rx_Buff_Sz = 2048;
+    static void Transmit(Serial* self);
 
     UART_HandleTypeDef* uart;
     RingBuffer<link_packet_t> rx_packets;
-    volatile bool tx_is_free;
-    uint8_t rx_buff[Rx_Buff_Sz];
-    link_packet_t* rx_packet;
-    uint16_t write_idx;
-    uint16_t read_idx;
+
+    uint8_t* tx_buff;
+    uint32_t tx_buff_sz;
+    uint8_t* rx_buff;
+    uint32_t rx_buff_sz;
+
+    uint16_t tx_write_idx;
+    uint16_t tx_read_idx;
+    bool tx_free;
+    uint16_t unsent;
+    uint16_t num_to_send;
+
+    uint16_t rx_write_idx;
+    uint16_t rx_read_idx;
     uint16_t unread;
 };
