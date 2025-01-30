2cdb4419bfb72fabe03bcfc69f73f136365f3271
Brett Regnier <bretternestregnier@gmail.com>
2025-01-30 16:48:14 -0700

    feat(ui): Add better error function so we can identify who called the error

--- firmware/ui/inc/app_main.hh
+++ firmware/ui/inc/app_main.hh
@@ -60,6 +60,7 @@ inline void ProcessText(uint16_t len);
 inline void InitScreen();
 inline void WaitForNetReady();
 inline void AudioCallback();
+inline void Error(const char* who, const char* why);
 
 void SlowSendTest(int delay, int num);
 void InterHactarSerialRoundTripTest(int delay, int num);
