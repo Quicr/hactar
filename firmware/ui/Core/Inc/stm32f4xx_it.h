42c9beaa19aec273ba62d243ff89884648cc2bd9
Brett Regnier <bretternestregnier@gmail.com>
2025-02-12 16:26:36 -0700

    Chore: Fixed serial framing errors

--- firmware/ui/Core/Inc/stm32f4xx_it.h
+++ firmware/ui/Core/Inc/stm32f4xx_it.h
@@ -59,6 +59,7 @@ void RCC_IRQHandler(void);
 void DMA1_Stream0_IRQHandler(void);
 void DMA1_Stream5_IRQHandler(void);
 void DMA1_Stream6_IRQHandler(void);
+void TIM3_IRQHandler(void);
 void SPI1_IRQHandler(void);
 void USART1_IRQHandler(void);
 void USART2_IRQHandler(void);
