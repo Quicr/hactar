94ec07b123e42fb343a2fe5d77ed2aa2cae33283
Finn Berg <finberg@cisco.com>
2025-05-07 09:07:32 -0700

    Replaced side buttons, updated capacitor part #, removed extra 1k resistor, switched connectors to new part #

--- hardware/hactar_diff.py
+++ hardware/hactar_diff.py
@@ -2,11 +2,11 @@ import os
 
 path = "EV13"
 
-input("Oldify the pdf?")
+x=input("Oldify the pdf?")
 os.system(f"rm ./{path}/old.pdf")
 os.system(f"cp ./{path}/'EV13 Board Design.pdf' ./{path}/old.pdf")
 
-input("Run the image diff?")
+x=input("Run the image diff?")
 os.system(f"magick convert -density 300 ./{path}/old.pdf -quality 50 -depth 16 ./{path}/new.png")
 os.system(f"magick convert -density 300 ./{path}/'EV13 Board Design.pdf' -quality 50 -depth 16 ./{path}/new.png")
 os.system(f"magick compare ./{path}/old.png ./{path}/new.png ./{path}/diff.png")
