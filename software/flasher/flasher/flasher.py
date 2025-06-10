9769149f0324f2b5a69f6d307495024c684fe1b6
Brett Regnier <bretternestregnier@gmail.com>
2025-06-10 16:07:17 -0400

    Flasher update (#258)

---
+++ software/flasher/flasher/flasher.py
@@ -0,0 +1,314 @@
+import argparse
+import sys
+import os
+import glob
+import time
+from ansi_colours import BB, BG, BR, BW, BY, NW
+
+# Get pyserial from our local copy
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "vendor"))
+import uart_utils
+import stm32
+import esp32
+import serial
+import serial.tools.list_ports
+
+
+def main():
+    try:
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument(
+            "-p",
+            "--port",
+            help="COM/Serial port that Hactar is on, leave blank to search for Hactars",
+            default="",
+            required=False,
+        )
+        parser.add_argument(
+            "-b",
+            "--baud",
+            help="Baudrate to communicate at",
+            default=115200,
+            type=int,
+            required=False,
+        )
+        parser.add_argument(
+            "-c",
+            "--chip",
+            help="Chips that are to be flashed to. "
+            "Available values: ui, net, mgmt. "
+            "Multiple chips: ui+net, or ui+net+mgmt, etc",
+            default="",
+            required=True,
+        )
+        parser.add_argument(
+            "-bin",
+            "--binary_path",
+            help="Path to the binary",
+            default="",
+            required=False,
+        )
+        parser.add_argument(
+            "-e",
+            "--use_external_flasher",
+            help="Gets hactar into flashing mode and then exits so a 3rd party flasher can be used",
+            default=False,
+            required=False,
+        )
+
+        args = parser.parse_args()
+
+        if not args.use_external_flasher and args.binary_path == "":
+            parser.error(
+                "A binary path must be provided if the flasher is not being used to get hactar chips into bootloder modes."
+            )
+
+        uart = None
+
+        uart_config = {
+            "baudrate": args.baud,
+            "bytesize": serial.EIGHTBITS,
+            "parity": serial.PARITY_NONE,
+            "stopbits": serial.STOPBITS_ONE,
+            "timeout": 2,
+        }
+
+        ports = []
+        if args.port == "":
+            # Try to find a hactar
+            print("Searching for Hactar devices")
+            ports = SerialPorts(uart_config)
+        else:
+            ports.append(args.port)
+
+        print(f"Uploading to {len(ports)} Hactar devices on ports: {ports}")
+
+        num_attempts = 5
+        i = 0
+        for port in ports:
+            flashed = False
+            while not flashed and i < num_attempts:
+                # flashed = True
+                i += 1
+                try:
+                    uart = serial.Serial(port=port, **uart_config)
+
+                    print(
+                        f"Opened port: {BB}{port}{NW} " f"baudrate: {BG}{args.baud}{NW}"
+                    )
+
+                    if "mgmt" in args.chip:
+                        print(f"{BW}Starting MGMT Upload{NW}")
+                        uart = FlashSelection(
+                            uart, args.chip, port, args.baud, args.use_external_flasher
+                        )
+                        stm32_flasher = stm32.stm32_flasher(uart)
+                        flashed = FlashHactarSTM32Chip(
+                            stm32_flasher, args.chip, args.binary_path, False
+                        )
+
+                    if "ui" in args.chip:
+                        print(f"{BW}Starting UI Upload{NW}")
+                        uart = FlashSelection(
+                            uart, args.chip, port, args.baud, args.use_external_flasher
+                        )
+                        stm32_flasher = stm32.stm32_flasher(uart)
+                        flashed = FlashHactarSTM32Chip(
+                            stm32_flasher, args.chip, args.binary_path, True
+                        )
+
+                    if "net" in args.chip:
+                        print(f"{BW}Starting Net Upload{NW}")
+                        uart = FlashSelection(
+                            uart, args.chip, port, args.baud, args.use_external_flasher
+                        )
+                        esp32_flasher = esp32.esp32_flasher(uart)
+                        flashed = esp32_flasher.FlashESP32S3Chip(args.binary_path)
+
+                    print(f"Done Flashing {BR}GOODBYE{NW}")
+                    uart.close()
+                except Exception as ex:
+                    print(f"{BR}[Error]{NW} {ex}, will try again")
+                    uart.close()
+                    time.sleep(3)
+            # End while
+    except Exception as ex:
+        print(f"{BR}[Error]{NW} {ex}")
+
+
+def SerialPorts(uart_config):
+    HELLO = bytes("WHO ARE YOU?\0", "utf-8")
+    HELLO_RES = bytes("HELLO, I AM A HACTAR DEVICE\0", "utf-8")
+
+    # Get all ports
+    ports = []
+    if sys.platform.startswith("win"):
+        coms = list(serial.tools.list_ports.comports())
+        ports = [port for (port, _, _) in coms]
+    elif sys.platform.startswith("linux") or sys.platform.startswith("cygwin"):
+        ports = glob.glob("/dev/ttyUSB[0-9]*")
+    elif sys.platform.startswith("darwin"):
+        ports = glob.glob("/dev/cu.usbserial*")
+    else:
+        raise EnvironmentError("Unsupported platform")
+    ports.sort()
+
+    print(f"Ports available: {len(ports)} [{ports}]")
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(**uart_config, port=port)
+            s.timeout = 0.5
+            # Send a message to the serial port
+            # If it responds with I AM A HACTAR DEVICE
+            # append it.
+            s.write(HELLO)
+
+            resp = s.read(len(HELLO_RES))
+
+            s.close()
+
+            if resp == HELLO_RES:
+                print(f"Device on port {BY}{port}{NW} {BG}is{NW} a Hactar!")
+                result.append(port)
+            else:
+                print(f"Device on port {BY}{port}{NW} {BR}not{NW} a Hactar!")
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+def FlashSelection(
+    uart: serial.Serial, chip: str, port: str, baud: int, use_external_flasher: bool
+) -> serial.Serial:
+    if chip.lower() == "mgmt":
+        uart.parity = serial.PARITY_EVEN
+        print(f"Updated uart to parity: {BB}EVEN{NW}")
+        print(f"User, put Hactar into bootloader mode!!")
+        input("Press enter once it is done...")
+        uart.reset_input_buffer()
+    elif chip.lower() == "ui":
+        send_data = [ch for ch in bytes("ui_upload", "UTF-8")]
+        uart.write(send_data)
+        uart.flush()
+
+        uart.close()
+        print(f"Update uart to parity: {BB}EVEN{NW}")
+
+        uart_config = {
+            "baudrate": baud,
+            "bytesize": serial.EIGHTBITS,
+            "parity": serial.PARITY_EVEN,
+            "stopbits": serial.STOPBITS_ONE,
+            "timeout": 2,
+        }
+
+        time.sleep(1)
+        uart = serial.Serial(port=port, **uart_config)
+        uart.reset_input_buffer()
+
+        uart_utils.TryHandshake(uart, uart_utils.OK, 1, 10)
+        uart_utils.TryPattern(uart, uart_utils.READY, 1, 10)
+
+        uart.reset_input_buffer()
+        print(f"Activating UI Upload Mode: {BG}SUCCESS{NW}")
+    elif chip.lower() == "net":
+        send_data = [ch for ch in bytes("net_upload", "UTF-8")]
+        uart.write(send_data)
+        uart.flush()
+
+        uart.close()
+        print(f"Update uart to parity: {BB}NONE{NW}")
+
+        uart_config = {
+            "baudrate": baud,
+            "bytesize": serial.EIGHTBITS,
+            "parity": serial.PARITY_NONE,
+            "stopbits": serial.STOPBITS_ONE,
+            "timeout": 2,
+        }
+
+        time.sleep(1)
+        uart = serial.Serial(port=port, **uart_config)
+        uart.reset_input_buffer()
+
+        uart_utils.TryHandshake(uart, uart_utils.OK, 1, 10)
+        uart_utils.TryPattern(uart, uart_utils.READY, 1, 10)
+
+        uart.reset_input_buffer()
+        print(f"Activating NET Upload Mode: {BG}SUCCESS{NW}")
+
+    if use_external_flasher:
+        print(f"Flasher has gotten {chip} on hactar into upload mode.")
+        exit(0)
+
+    return uart
+
+
+def RecoverFlashSelection(flasher, chip, recover):
+    trying_to_select = True
+    while trying_to_select:
+        try:
+            flasher.uart.close()
+            flasher.uart.timeout = 2
+            flasher.uart.parity = serial.PARITY_NONE
+            # Timeout period for mgmt
+            time.sleep(5)
+            flasher.uart.open()
+            uart_utils.FlashSelection(flasher.uart, chip)
+            trying_to_select = False
+        except Exception as ex:
+            if not recover:
+                raise ex
+
+
+def RecoverableEraseMemory(flasher, sectors, chip, recover):
+    finished_erasing = False
+    while not finished_erasing:
+        try:
+            finished_erasing = flasher.SendExtendedEraseMemory(
+                sectors, False, True, True
+            )
+        except Exception as ex:
+            if not recover:
+                raise ex
+            print(ex)
+            print(f"Erase: {BB}Recovery mode{NW}")
+            time.sleep(3)
+            RecoverFlashSelection(flasher, chip, recover)
+
+
+def RecoverableFlashMemory(flasher, firmware, chip, recover):
+    finished_writing = False
+    while not finished_writing:
+        try:
+            # TODO add a function for getting the start of the address?
+            finished_writing = flasher.SendWriteMemory(
+                firmware, flasher.chip_config["usr_start_addr"], recover
+            )
+        except Exception as ex:
+            if not recover:
+                raise ex
+            print(ex)
+            print(f"Flashing: {BB}Recovery mode{NW}")
+            time.sleep(3)
+            RecoverFlashSelection(flasher, chip, recover)
+
+
+def FlashHactarSTM32Chip(flasher, chip, binary_path, recover):
+
+    # Get the firmware
+    firmware = open(binary_path, "rb").read()
+
+    # Get the size of memory that we need to erase
+    sectors = flasher.GetSectorsForFirmware(len(firmware))
+
+    RecoverableEraseMemory(flasher, sectors, chip, recover)
+
+    RecoverableFlashMemory(flasher, firmware, chip, recover)
+
+    if chip == "mgmt":
+        flasher.SendGo(flasher.chip_config["usr_start_addr"])
+
+    return True
