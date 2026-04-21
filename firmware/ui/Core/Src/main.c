ea468f422dd21c6a262b1af3c749ee4de8f65056
Brett Regnier <bretternestregnier@gmail.com>
2026-04-21 13:41:39 -0600

    Updated timer to tick every 1ms

--- firmware/ui/Core/Src/main.c
+++ firmware/ui/Core/Src/main.c
@@ -373,9 +373,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 16800;
+  htim2.Init.Prescaler = 839;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 200;
+  htim2.Init.Period = 100;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
